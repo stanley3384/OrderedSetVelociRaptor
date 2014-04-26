23afa6814048f32aadc1627b7f3ee7300a0aa0be
C. Eric Cashon <cecashon@aol.com>
2014-04-26 09:11:30 -0700

    Fix label overhang on microtiter platemap.

--- Misc/heatmap1.py
+++ Misc/heatmap1.py
@@ -11,14 +11,15 @@ import sys
 import random
 import os
 
-#Add 1 for column for labels. Used for the HTML layout.
+#Add 1 to columns for labels. The first column in a row is a label.
 rows = 8
 columns = 12 + 1
 plates = 10
 plate_size = 96
 
 row_labels = ["A","B","C","D","E","F","G","H"]
-column_labels = ["1","2","3","4","5","6","7","8","9","10","11","12",""]
+column_labels = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", ""]
+
 gradient_iris = ["#d507e5", "#d60fde", "#d711dc", "#d716d7", "#d819d5", "#d91ed1", "#d920ce", "#da25ca", "#da28c7", "#db2dc3", "#dc2fc1", "#dd34bc", "#dd37ba", "#de3cb5", "#de3eb3", "#df43ae", "#df46ac", "#e04ba8", "#e14da5", "#e252a1", "#e2559e", "#e35c98", "#e46193", "#e46491", "#e5698c", "#e77085", "#e77383", "#e8787f", "#e97f78", "#ea8275", "#ea8771", "#eb896f", "#ec8e6a", "#ec9168", "#ed9663", "#ed9861", "#ee9d5c", "#efa05a", "#efa556", "#f0a753", "#f1ac4f", "#f1af4c", "#f2b448", "#f2b646", "#f3bb41", "#f4be3f", "#f4c33a", "#f5c538", "#f6ca33", "#f6cd31", "#f7d22d", "#f7d42a", "#f8d926", "#f9dc23", "#f9e11f", "#fae31d", "#fbe818", "#fbeb16", "#fcf011", "#fcf20f", "#fdf50d", "#fdf70a", "#fefa08", "#fefc06", "#fefe04"]
 
 #Get some test data.
@@ -39,7 +40,7 @@ f.write("<meta charset=\"UTF-8\"/>")
 f.write("<title>Heatmap</title>")
 f.write("</head>")
 f.write("<body>")
-f.write("<h1>Some 96 Well Plates</h1>")
+f.write("<h1>Some 384 Well Plates</h1>")
 
 #Use a counter for simplicity.
 counter = 0
@@ -48,8 +49,8 @@ for i in range(0, plates, 1):
     f.write("<p></p>")
     f.write("<table><caption>Plate %s</caption><thead><tr>" % (str(i+1)))
     f.write("<th scope=\"rowgroup\" bgcolor=\"silver\"></th>")
-    for column in column_labels:
-        f.write("<th scope=\"col\" bgcolor=\"silver\">%s</th>" % (column))
+    for j in range(0, columns-1, 1):
+        f.write("<th scope=\"col\" bgcolor=\"silver\">%s</th>" % (column_labels[j]))
     f.write("</tr></thead><tbody>")  
 
     #Write the data and color gradient into the table.
