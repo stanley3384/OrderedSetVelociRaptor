62d480cc9162efd8287fbd6dfce6a6b0f52d3203
C. Eric Cashon <cecashon@aol.com>
2014-04-30 09:22:41 -0700

    Heatmap and format 384 well plates.

--- Misc/heatmap1.py
+++ Misc/heatmap1.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 '''
+                                  Heatmaps are Cool
    A simple example of formatting some microtiter plates with Python and HTML. Change things
 around as needed. Can even get inventive with CSS or stream data from a database. Open the 
 resulting file in a HTML5 browser.
@@ -13,20 +14,25 @@ import random
 import os
 import sqlite3 as lite
 
-#Add 1 to columns for labels. The first column in a row is a label.
+'''
+    Add 1 to columns for labels. The first column in a row is a label. Can test out 384 well
+plates also. Need to change the following variables though. For bigger than 384 the row_labels
+will need to be extended.
+'''
 rows = 8
 columns = 12 + 1
 plates = 10
 plate_size = 96
 
-row_labels = ["A","B","C","D","E","F","G","H"]
-column_labels = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
+#Enough row labels for 96 or 384 well plates
+row_labels = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P"]
 
+#Get a color gradient from KColorEdit.
 gradient_iris = ["#d507e5", "#d60fde", "#d711dc", "#d716d7", "#d819d5", "#d91ed1", "#d920ce", "#da25ca", "#da28c7", "#db2dc3", "#dc2fc1", "#dd34bc", "#dd37ba", "#de3cb5", "#de3eb3", "#df43ae", "#df46ac", "#e04ba8", "#e14da5", "#e252a1", "#e2559e", "#e35c98", "#e46193", "#e46491", "#e5698c", "#e77085", "#e77383", "#e8787f", "#e97f78", "#ea8275", "#ea8771", "#eb896f", "#ec8e6a", "#ec9168", "#ed9663", "#ed9861", "#ee9d5c", "#efa05a", "#efa556", "#f0a753", "#f1ac4f", "#f1af4c", "#f2b448", "#f2b646", "#f3bb41", "#f4be3f", "#f4c33a", "#f5c538", "#f6ca33", "#f6cd31", "#f7d22d", "#f7d42a", "#f8d926", "#f9dc23", "#f9e11f", "#fae31d", "#fbe818", "#fbeb16", "#fcf011", "#fcf20f", "#fdf50d", "#fdf70a", "#fefa08", "#fefc06", "#fefe04"]
 
 #Get some test data.
 test_data=[]
-for i in range(0, 960, 1):
+for i in range(0, (plate_size*plates), 1):
     test_data.append(random.uniform(0,100))
 
 '''
@@ -57,6 +63,7 @@ for i in range(0, plates, 1):
     high.append(max(test_data[index1:index2]))   
     low.append(min(test_data[index1:index2]))
 
+#Open a file and write the HTML.
 f = open('heatmap1.html','w')
 
 f.write("<!DOCTYPE html>\n")
@@ -89,7 +96,7 @@ for i in range(0, plates, 1):
     f.write("<table align=\"center\" bgcolor=\"silver\"  style=\"margin-bottom:40pt\"><caption>Plate %s</caption><thead><tr>\n" % (str(i+1)))
     f.write("<th scope=\"rowgroup\" bgcolor=\"silver\"></th>\n")
     for j in range(0, columns-1, 1):
-        f.write("<th scope=\"col\" bgcolor=\"silver\">%s</th>\n" % (column_labels[j]))
+        f.write("<th scope=\"col\" bgcolor=\"silver\">%i</th>\n" % (j+1))
     f.write("</tr></thead><tbody>\n")  
 
     #Write the data and color gradient into the table.
