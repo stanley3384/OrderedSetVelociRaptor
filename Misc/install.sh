e074ebc9c46970ba32504c715ec46d23ffe5e77b
C. Eric Cashon <cecashon@aol.com>
2014-05-12 16:31:13 -0700

    Test an install script.

---
+++ Misc/install.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+#An install script for the VelociRaptor program.
+#This is still being tested!!! It is just a collection of the steps in README.
+#Make sure to take a look at README before trying this script. Also take a 
+#look at the script so you can see what it is doing.
+
+#To use, make a new directory and copy the install.sh file to the new directory.
+#A "test" directory would work fine. Then run the script.
+# bash install.sh
+# or 
+# ./install.sh
+
+read -p "Do you wish to install the VelociRaptor program? (y/n) " ANSWER
+if [ "$ANSWER" = "y" ]; then
+    sudo -v
+    #See if github is available.
+    ping -c 1 www.github.com
+    STATUS=$?
+    if [ $STATUS -ne 0 ]; then
+       echo "Can't find github. Make sure you have an active internet connection and retry."
+    else
+        echo "Make RaptorDirectoryTest"
+        rm -rf RaptorDirectoryTest
+        rm -f master.zip
+        mkdir RaptorDirectoryTest
+        cd ./RaptorDirectoryTest
+        echo "Get zip file"
+        wget https://github.com/cecashon/OrderedSetVelociRaptor/archive/master.zip
+        echo "Unzip"
+        unzip master.zip
+        echo "Move to Misc"
+        cd ./OrderedSetVelociRaptor-master/Misc
+        echo "Get dependencies"
+        sudo apt-get update
+        sudo apt-get install autoconf automake libtool 
+        sudo apt-get install make gcc libgsl0-dev libsqlite3-dev
+        sudo apt-get install libgtk-3-dev gnuplot gfortran
+        echo "Unpack Apophenia"
+        tar xzf apophenia-*.tgz
+        echo "Apophenia directory"
+        cd ./apophenia-0.99
+        echo "Configure"
+        ./configure
+        echo "Make"
+        make
+        echo "Make Install"
+        sudo make install
+        echo "VelociRaptor directory"
+        cd ../../
+        echo "Build the program"
+        make
+        echo "Run with ./VelociRaptor"
+        ./VelociRaptor
+    fi
+else
+  echo "Exit Install"
+fi
+
+
