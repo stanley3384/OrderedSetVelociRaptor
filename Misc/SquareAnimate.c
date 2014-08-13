1ecbf428087dd5bd2647a188e4783f7d2b42c4bf
C. Eric Cashon <cecashon@aol.com>
2014-08-13 16:18:07 -0700

    Change cairo code in SquareAnimate.c

--- Misc/SquareAnimate.c
+++ Misc/SquareAnimate.c
@@ -5,7 +5,7 @@ Simple animation with GTK+ and Cairo. Click to start movement. Testing things ou
 Roll a square, build a cycloid "bridge" wave, sine wave and cosine wave. Have the
 square disappear and reappear. Magic Square. Change around as needed.
 
-Compile with; gcc SquareAnimate.c `pkg-config --cflags --libs gtk+-3.0` -lm -Wall -o square
+Compile with; gcc -Wall `pkg-config --cflags gtk+-3.0` SquareAnimate.c -o square `pkg-config --libs gtk+-3.0` -lm
 
 C. Eric Cashon
 
@@ -17,10 +17,10 @@ C. Eric Cashon
 int move=1001;
 
 static void close_window(GtkWidget *widget, gpointer data);
-static void start_drawing(GtkWidget *widget, gpointer data);
+static gboolean start_drawing(GtkWidget *widget, cairo_t *cr, gpointer data);
 static void click_drawing(GtkWidget *widget, gpointer data);
 static void realize_drawing(GtkWidget *widget, gpointer data);
-static void draw_square(GtkWidget *widget, int move);
+static gboolean draw_square(GtkWidget *widget, cairo_t *cr, int move);
 
 int main (int argc, char *argv[])
  {
@@ -32,7 +32,7 @@ int main (int argc, char *argv[])
     g_signal_connect(window, "destroy", G_CALLBACK(close_window), NULL);
 
     GtkWidget *SquareDrawing = gtk_drawing_area_new();
-    gtk_widget_set_size_request(SquareDrawing, 800, 350);
+    gtk_widget_set_size_request(SquareDrawing, 950, 350);
     gtk_widget_set_events(SquareDrawing, GDK_BUTTON_PRESS_MASK);
     g_signal_connect(G_OBJECT(SquareDrawing), "button_press_event", G_CALLBACK(click_drawing), NULL); 
     g_signal_connect(G_OBJECT(SquareDrawing), "draw", G_CALLBACK(start_drawing), NULL); 
@@ -49,35 +49,33 @@ static void close_window(GtkWidget *widget, gpointer data)
   {
     gtk_main_quit ();
   }
-static void start_drawing(GtkWidget *widget, gpointer data)
+static gboolean start_drawing(GtkWidget *widget, cairo_t *cr, gpointer data)
   {
-    int i=0;
     move+=5;
-
-    //Move drawing area to reduce re-draw area.
-    if(move<640){i=move-5;}
-    else if(move>=640&&move<=1000){i=640;}
-    else{i=0;}
+    GtkAllocation allocation;
+    GdkWindow *win=gtk_widget_get_window(widget);
+    gtk_widget_get_allocation(GTK_WIDGET(widget), &allocation);
 
     if(move<=1000)
       {
         if(move!=1000)
           {
-            draw_square(widget, move);
-            gtk_widget_queue_draw_area(widget, i, 30, 170, 350);
+            draw_square(widget, cr, move);
+            gdk_window_invalidate_rect(win, &allocation, FALSE);
           }
         else
           {
-            draw_square(widget, 0);
-            gtk_widget_queue_draw_area(widget, 0, 0, 800, 350);
+            draw_square(widget, cr, 0);
+            gdk_window_invalidate_rect(win, &allocation, FALSE);
           }
       }  
     else
       {
-        draw_square(widget, 0);        
+        draw_square(widget, cr, 0);        
       }
         
-     g_usleep(20000);
+     g_usleep(30000);
+     return TRUE;
       
   }
 static void realize_drawing(GtkWidget *widget, gpointer data)
@@ -87,111 +85,95 @@ static void realize_drawing(GtkWidget *widget, gpointer data)
 static void click_drawing(GtkWidget *widget, gpointer data)
   {
     move=0;
-    gtk_widget_queue_draw_area(widget, 0, 0, 800, 350);  
+    gtk_widget_queue_draw_area(widget, 0, 0, 950, 350);  
   }
-static void draw_square(GtkWidget *widget, int move)
+static gboolean draw_square(GtkWidget *widget, cairo_t *cr, int move)
   {  
     int i=0;
     double x1=0;
     double y1=0;
-    GdkWindow *DefaultWindow=NULL;
-    cairo_t *square=NULL;
-    cairo_t *circle=NULL;
-    cairo_t *line1=NULL;
-    cairo_t *line2=NULL;
-    cairo_t *line3=NULL;
-    cairo_t *line4=NULL;
-    cairo_t *line5=NULL;
-    cairo_t *line6=NULL;
 
     g_print("Draw Square %i\n", move);
-    DefaultWindow=gtk_widget_get_window(GTK_WIDGET(widget));
-    square = gdk_cairo_create(DefaultWindow);
-    circle = gdk_cairo_create(DefaultWindow);
-    line1 = gdk_cairo_create(DefaultWindow);
-    line2 = gdk_cairo_create(DefaultWindow);
-    line3 = gdk_cairo_create(DefaultWindow);
-    line4 = gdk_cairo_create(DefaultWindow);
-    line5 = gdk_cairo_create(DefaultWindow);
-    line6 = gdk_cairo_create(DefaultWindow);         
 
+    //Clear the surface.
+    cairo_save(cr);
+    cairo_set_source_rgba(cr, 1.0, 1.0, 1.0, 1.0);
+    cairo_paint(cr);
+    cairo_restore(cr);
+    
+    cairo_save(cr);
     //Rotate around center of square so move there.
-    cairo_translate(square, 30+move+50, 130+50);
+    cairo_translate(cr, 30+move+50, 130+50);
     //Rotate the square. Tip the square on end with pi/4 to start. 
-    cairo_rotate(square, (((move/10)*G_PI/25.0)+G_PI/4.0));
+    cairo_rotate(cr, (((move/10)*G_PI/25.0)+G_PI/4.0));
     //Move back to to the corner of the square and draw the square.
-    cairo_translate(square, -50, -50);
-    cairo_set_source_rgb(square, 0.5, 0, 0.5);
-    cairo_rectangle(square, 0, 0, 100.0, 100.0);
-    cairo_fill(square);
+    cairo_translate(cr, -50, -50);
+    cairo_set_source_rgb(cr, 0.5, 0, 0.5);
+    cairo_rectangle(cr, 0, 0, 100.0, 100.0);
+    cairo_fill(cr);
+
     //Add a circle around the square.
-    cairo_translate(circle, 30+move+50, 130+50);
-    cairo_set_source_rgb(circle, 1, 1, 0);
-    cairo_set_line_width(circle, 4.0);
-    cairo_arc(circle, 0, 0, sqrt(5000.0), 0, 2*M_PI);
-    cairo_close_path(circle);
-    cairo_stroke(circle);
+    cairo_translate(cr, 50, 50);
+    cairo_set_source_rgb(cr, 1, 1, 0);
+    cairo_set_line_width(cr, 4.0);
+    cairo_arc(cr, 0, 0, sqrt(5000.0), 0, 2*M_PI);
+    cairo_close_path(cr);
+    cairo_stroke(cr);
+    cairo_restore(cr);
+
     //Put a line in for the square to roll on.
-    cairo_set_source_rgb(line1, 0, 0, 0);
-    cairo_move_to(line1, 0, 130+50+sqrt(5000.0));
-    cairo_line_to(line1, 1000, 130+50+sqrt(5000.0));
-    cairo_set_line_width(line1, 3.0);
-    cairo_stroke(line1);
+    cairo_set_source_rgb(cr, 0, 0, 0);
+    cairo_move_to(cr, 0, 130+50+sqrt(5000.0));
+    cairo_line_to(cr, 1000, 130+50+sqrt(5000.0));
+    cairo_set_line_width(cr, 3.0);
+    cairo_stroke(cr);
+
     //Make a line in the square.
-    cairo_set_source_rgb(line2, 0, 1.0, 0);
-    cairo_set_line_width(line2, 2.0);
-    cairo_move_to(line2,30+50+move, 130+50);
-    cairo_arc(line2, 30+50+move, 130+50, sqrt(5000.0), (((move/10)*G_PI/25.0)+G_PI/2.0), (((move/10)*G_PI/25.0)+G_PI/2.0));
-    cairo_line_to(line2, 30+50+move, 130+50);
-    cairo_stroke(line2);
+    cairo_set_source_rgb(cr, 0, 1.0, 0);
+    cairo_set_line_width(cr, 2.0);
+    cairo_move_to(cr,30+50+move, 130+50);
+    cairo_arc(cr, 30+50+move, 130+50, sqrt(5000.0), (((move/10)*G_PI/25.0)+G_PI/2.0), (((move/10)*G_PI/25.0)+G_PI/2.0));
+    cairo_line_to(cr, 30+50+move, 130+50);
+    cairo_stroke(cr);
+
     //Set line colors and width.
-    cairo_set_line_width(line3, 2.0);
-    cairo_set_source_rgb(line3, 0, 1.0, 0);
-    cairo_set_line_width(line4, 2.0);
-    cairo_set_source_rgb(line4, 0, 0, 1.0);
-    cairo_set_line_width(line5, 1.0);
-    cairo_set_source_rgb(line5, 1, 0, 0);
-    cairo_set_line_width(line6, 40.0);
     for(i=0;i<move;i+=10)
        {
          //The bridge.
-         cairo_move_to(line3,30+50+i, 130+50);
-         cairo_arc(line3, 30+50+i, 130+50, sqrt(5000.0), (((i/10)*G_PI/25.0)+G_PI/2.0), (((i/10)*G_PI/25.0)+G_PI/2.0));
-         cairo_line_to(line3, 30+50+i, 130+50);
-         cairo_stroke(line3);
+         cairo_set_line_width(cr, 2.0);
+         cairo_set_source_rgb(cr, 0, 1.0, 0);
+         cairo_move_to(cr,30+50+i, 130+50);
+         cairo_arc(cr, 30+50+i, 130+50, sqrt(5000.0), (((i/10)*G_PI/25.0)+G_PI/2.0), (((i/10)*G_PI/25.0)+G_PI/2.0));
+         cairo_line_to(cr, 30+50+i, 130+50);
+         cairo_stroke(cr);
          //The sine wave.
+         cairo_set_source_rgb(cr, 0, 0, 1.0);
          y1=(sqrt(5000.0))*sin(((i/10.0)*G_PI/25.0)+G_PI/2.0);
-         cairo_move_to(line4, 30+50+i, 130+50);
-         cairo_line_to(line4, 30+50+i, 130+50+(int)y1);
-         cairo_stroke(line4);
+         cairo_move_to(cr, 30+50+i, 130+50);
+         cairo_line_to(cr, 30+50+i, 130+50+(int)y1);
+         cairo_stroke(cr);
          //The cosine wave. Rotate slightly off the x-axis.
+         cairo_set_source_rgb(cr, 1, 0, 0);
          x1=(sqrt(5000.0))*cos(((i/10.0)*G_PI/25.0)+G_PI/2.0);
          if(x1>=0)
            {
-             cairo_arc(line5, 30+50+i, 130+50, (int)floor(x1), G_PI/8, G_PI/8);
+             cairo_arc(cr, 30+50+i, 130+50, (int)floor(x1), G_PI/8, G_PI/8);
            }
          else
            {
-             cairo_arc(line5, 30+50+i, 130+50, (int)floor(fabs(x1)), G_PI+G_PI/8, G_PI+G_PI/8);
+             cairo_arc(cr, 30+50+i, 130+50, (int)floor(fabs(x1)), G_PI+G_PI/8, G_PI+G_PI/8);
            }
-         cairo_line_to(line5, 30+50+i, 130+50);
-         cairo_stroke(line5); 
-         //The sine cosine color component line. 
-         cairo_set_source_rgb(line6, fabs(x1)/sqrt(5000), 0, fabs(y1)/sqrt(5000)); 
-         cairo_move_to(line6, 30+50+i, 130+50+110);
-         cairo_line_to(line6, 30+50+i+10, 130+50+110);
-         cairo_stroke(line6);       
+         cairo_line_to(cr, 30+50+i, 130+50);
+         cairo_stroke(cr); 
+         //The sine cosine color component line.
+         cairo_set_line_width(cr, 40.0); 
+         cairo_set_source_rgb(cr, fabs(x1)/sqrt(5000), 0, fabs(y1)/sqrt(5000)); 
+         cairo_move_to(cr, 30+50+i, 130+50+110);
+         cairo_line_to(cr, 30+50+i+10, 130+50+110);
+         cairo_stroke(cr);       
        }
-
-    //clean-up.
-    cairo_destroy(square);
-    cairo_destroy(circle);
-    cairo_destroy(line1);
-    cairo_destroy(line2);
-    cairo_destroy(line3);
-    cairo_destroy(line4);
-    cairo_destroy(line5);
-    cairo_destroy(line6);
+  
+    return TRUE;
 
   }
 
