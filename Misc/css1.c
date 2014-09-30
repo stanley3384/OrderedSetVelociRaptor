f54e13a1fa30649658c5260ec051a55571a230a8
C. Eric Cashon <cecashon@aol.com>
2014-09-30 10:39:37 -0700

    Add function prototypes to css1.c

--- Misc/css1.c
+++ Misc/css1.c
@@ -28,6 +28,138 @@ GTimer *timer2=NULL;
 guint width_t=0;
 guint height_t=0;
 
+static gboolean change_font_color_enter(GtkWidget *button, GdkEvent *event, GtkLabel *label);
+static gboolean change_font_color_leave(GtkWidget *button, GdkEvent *event, GtkLabel *label);
+static gboolean change_font_color_enter_event(GtkWidget *event_box, GdkEvent *event, GtkLabel *label);
+static gboolean change_font_color_leave_event(GtkWidget *event_box, GdkEvent *event, GtkLabel *label);
+static gboolean event_box_button_press(GtkWidget *event_box, GdkEvent *event, GtkLabel *label);
+gboolean draw_radial_color(GtkWidget *widget, cairo_t *cr, gpointer data);
+static gboolean end_thread(GThread *thread);
+static void *draw_radial_color_t2(cairo_surface_t *cairo_surface1);
+gboolean draw_radial_color_t1(GtkWidget *widget, cairo_t *cr, gpointer data);
+static gboolean animate_drawing_area(gpointer data);
+gboolean select_draw_path(GtkWidget *widget, cairo_t *cr, gpointer data);
+static void set_threading_path(GtkWidget *combo1, gpointer *data);
+static void set_sleep(GtkWidget *combo2, gpointer *data);
+static void close_program();
+
+int main(int argc, char **argv)
+ {
+   GtkWidget *window, *combo1, *combo2, *label1, *label2, *button1, *button2, *button3, *button4, *button_label1, *button_label2, *button_label3, *button_label4, *event_box1, *drawing_area1, *grid1;
+   gchar css_string[]="GtkButton{background: blue}\n\
+                       GtkLabel{color: black}\n\
+                       GtkComboBoxText{color: white}\n\
+                       GtkEventBox{background: yellow}\n\
+                       GtkButton#css_button3{background-image: -gtk-gradient (linear, left center, right center, color-stop(0.0,rgba(255,0,0,1)), color-stop(0.5,rgba(0,255,0,1)), color-stop(1.0,rgba(0,0,255,1)));}\n\
+                       GtkButton#css_button4{background-image: -gtk-gradient (linear, left center, right center, color-stop(0.0,rgba(255,0,255,1)), color-stop(1.0,rgba(255,255,0,1)));}\n\
+                       GtkButton#css_button3:focused{background: purple}\n\
+                       GtkButton#css_button4:focused{background: purple}\n\
+                       GtkWindow{background-image: -gtk-gradient (linear, left center, right center, color-stop(0.0,rgba(255,0,255,1)), color-stop(1.0,rgba(255,255,0,1)));}\n\
+                       GtkButton:focused{background: purple}";
+   GtkCssProvider *provider;
+   GdkDisplay *display;
+   GdkScreen *screen;
+   GError *css_error=NULL;
+
+   gtk_init(&argc, &argv);
+
+   window=gtk_window_new(GTK_WINDOW_TOPLEVEL);
+   gtk_window_set_title(GTK_WINDOW(window), "Thread Test");
+   gtk_window_set_default_size(GTK_WINDOW(window), 300, 300);
+   gtk_container_set_border_width(GTK_CONTAINER(window), 10);
+   g_signal_connect_swapped(window, "destroy", G_CALLBACK(close_program), NULL);
+
+   combo1=gtk_combo_box_text_new();     
+   gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(combo1), "0", "Single Thread");
+   gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(combo1), "1", "Single Thread T_path");
+   gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(combo1), "2", "Worker Thread T_path");
+   gtk_combo_box_set_active(GTK_COMBO_BOX(combo1), 2);
+   g_signal_connect(combo1, "changed", G_CALLBACK(set_threading_path), NULL);
+
+   combo2=gtk_combo_box_text_new();     
+   gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(combo2), "0", "Timer 0 Second");
+   gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(combo2), "1", "Timer 1/2 Second");
+   gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(combo2), "2", "Timer 1 Seconds");
+   gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(combo2), "3", "Timer 2 Seconds");
+   gtk_combo_box_set_active(GTK_COMBO_BOX(combo2), 1);
+   g_signal_connect(combo2, "changed", G_CALLBACK(set_sleep), NULL);
+     
+   label1=gtk_label_new("label1");
+   gtk_widget_set_hexpand(label1, TRUE);
+
+   label2=gtk_label_new("label2");
+   gtk_widget_set_hexpand(label2, TRUE);
+   gtk_widget_set_name (GTK_WIDGET(label2), "css_label2"); 
+
+   event_box1=gtk_event_box_new();
+   gtk_widget_set_hexpand(event_box1, TRUE);
+   gtk_container_add(GTK_CONTAINER(event_box1), label2);
+   gtk_widget_set_events(event_box1, GDK_ENTER_NOTIFY_MASK);
+   gtk_widget_set_events(event_box1, GDK_LEAVE_NOTIFY_MASK);
+   gtk_widget_set_events(event_box1, GDK_BUTTON_PRESS_MASK);
+   g_signal_connect(event_box1, "enter-notify-event", G_CALLBACK(change_font_color_enter_event), GTK_LABEL(label2));
+   g_signal_connect(event_box1, "leave-notify-event", G_CALLBACK(change_font_color_leave_event), GTK_LABEL(label2)); 
+   g_signal_connect(event_box1, "button-press-event", G_CALLBACK(event_box_button_press), GTK_LABEL(label2)); 
+
+   button1=gtk_button_new_with_label("button1");
+   gtk_widget_set_hexpand(button1, TRUE);
+   button_label1=gtk_bin_get_child(GTK_BIN(button1));
+   g_signal_connect(button1, "enter-notify-event", G_CALLBACK(change_font_color_enter), GTK_LABEL(button_label1));
+   g_signal_connect(button1, "leave-notify-event", G_CALLBACK(change_font_color_leave), GTK_LABEL(button_label1));
+
+   button2=gtk_button_new_with_label("button2");
+   gtk_widget_set_hexpand(button2, TRUE);
+   button_label2=gtk_bin_get_child(GTK_BIN(button2));
+   g_signal_connect(button2, "enter-notify-event", G_CALLBACK(change_font_color_enter), GTK_LABEL(button_label2));
+   g_signal_connect(button2, "leave-notify-event", G_CALLBACK(change_font_color_leave), GTK_LABEL(button_label2));
+
+   button3=gtk_button_new_with_label("button3");
+   gtk_widget_set_hexpand(button3, TRUE);
+   button_label3=gtk_bin_get_child(GTK_BIN(button3));
+   g_signal_connect(button3, "enter-notify-event", G_CALLBACK(change_font_color_enter), GTK_LABEL(button_label3));
+   g_signal_connect(button3, "leave-notify-event", G_CALLBACK(change_font_color_leave), GTK_LABEL(button_label3));
+   gtk_widget_set_name(GTK_WIDGET(button3), "css_button3"); 
+
+   button4=gtk_button_new_with_label("button4");
+   gtk_widget_set_hexpand(button4, TRUE);
+   button_label4=gtk_bin_get_child(GTK_BIN(button4));
+   g_signal_connect(button4, "enter-notify-event", G_CALLBACK(change_font_color_enter), GTK_LABEL(button_label4));
+   g_signal_connect(button4, "leave-notify-event", G_CALLBACK(change_font_color_leave), GTK_LABEL(button_label4));
+   gtk_widget_set_name(GTK_WIDGET(button4), "css_button4");    
+
+   drawing_area1=gtk_drawing_area_new();
+   gtk_widget_set_size_request (drawing_area1, 300, 150);
+   gtk_widget_set_vexpand(drawing_area1, TRUE);
+   g_signal_connect(G_OBJECT(drawing_area1), "draw", G_CALLBACK(select_draw_path), NULL);
+   
+   grid1=gtk_grid_new();
+   gtk_container_add(GTK_CONTAINER(window), grid1);
+
+   gtk_grid_attach(GTK_GRID(grid1), combo1, 0, 0, 1, 1);
+   gtk_grid_attach(GTK_GRID(grid1), combo2, 0, 1, 1, 1);
+   gtk_grid_attach(GTK_GRID(grid1), label1, 0, 2, 1, 1);
+   gtk_grid_attach(GTK_GRID(grid1), event_box1, 0, 3, 1, 1);
+   gtk_grid_attach(GTK_GRID(grid1), button1, 0, 4, 1, 1);
+   gtk_grid_attach(GTK_GRID(grid1), button2, 0, 5, 1, 1);
+   gtk_grid_attach(GTK_GRID(grid1), button3, 0, 6, 1, 1);
+   gtk_grid_attach(GTK_GRID(grid1), button4, 0, 7, 1, 1);
+   gtk_grid_attach(GTK_GRID(grid1), drawing_area1, 0, 8, 3, 1);
+   
+   provider = gtk_css_provider_new();
+   display = gdk_display_get_default();
+   screen = gdk_display_get_default_screen(display);
+   gtk_style_context_add_provider_for_screen(screen, GTK_STYLE_PROVIDER(provider), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+   gtk_css_provider_load_from_data(provider, css_string, -1, &css_error);
+   if(css_error!=NULL) g_print("CSS loader error %s\n", css_error->message);
+   g_object_unref(provider);
+
+   gtk_widget_show_all(window);
+
+   timer_id=gdk_threads_add_timeout(100, animate_drawing_area, drawing_area1);
+
+   gtk_main();
+   return 0;
+ }
 static gboolean change_font_color_enter(GtkWidget *button, GdkEvent *event, GtkLabel *label)
  {
    gchar *string=g_strdup_printf( "<span foreground=\"white\">%s</span>", gtk_button_get_label(GTK_BUTTON(button)));
@@ -260,124 +392,4 @@ static void close_program()
    printf("Quit Program\n");
    gtk_main_quit();
  }
-int main(int argc, char **argv)
- {
-   GtkWidget *window, *combo1, *combo2, *label1, *label2, *button1, *button2, *button3, *button4, *button_label1, *button_label2, *button_label3, *button_label4, *event_box1, *drawing_area1, *grid1;
-   gchar css_string[]="GtkButton{background: blue}\n\
-                       GtkLabel{color: black}\n\
-                       GtkComboBoxText{color: white}\n\
-                       GtkEventBox{background: yellow}\n\
-                       GtkButton#css_button3{background-image: -gtk-gradient (linear, left center, right center, color-stop(0.0,rgba(255,0,0,1)), color-stop(0.5,rgba(0,255,0,1)), color-stop(1.0,rgba(0,0,255,1)));}\n\
-                       GtkButton#css_button4{background-image: -gtk-gradient (linear, left center, right center, color-stop(0.0,rgba(255,0,255,1)), color-stop(1.0,rgba(255,255,0,1)));}\n\
-                       GtkButton#css_button3:focused{background: purple}\n\
-                       GtkButton#css_button4:focused{background: purple}\n\
-                       GtkWindow{background-image: -gtk-gradient (linear, left center, right center, color-stop(0.0,rgba(255,0,255,1)), color-stop(1.0,rgba(255,255,0,1)));}\n\
-                       GtkButton:focused{background: purple}";
-   GtkCssProvider *provider;
-   GdkDisplay *display;
-   GdkScreen *screen;
-   GError *css_error=NULL;
 
-   gtk_init(&argc, &argv);
-
-   window=gtk_window_new(GTK_WINDOW_TOPLEVEL);
-   gtk_window_set_title(GTK_WINDOW(window), "Thread Test");
-   gtk_window_set_default_size(GTK_WINDOW(window), 300, 300);
-   gtk_container_set_border_width(GTK_CONTAINER(window), 10);
-   g_signal_connect_swapped(window, "destroy", G_CALLBACK(close_program), NULL);
-
-   combo1=gtk_combo_box_text_new();     
-   gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(combo1), "0", "Single Thread");
-   gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(combo1), "1", "Single Thread T_path");
-   gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(combo1), "2", "Worker Thread T_path");
-   gtk_combo_box_set_active(GTK_COMBO_BOX(combo1), 2);
-   g_signal_connect(combo1, "changed", G_CALLBACK(set_threading_path), NULL);
-
-   combo2=gtk_combo_box_text_new();     
-   gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(combo2), "0", "Timer 0 Second");
-   gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(combo2), "1", "Timer 1/2 Second");
-   gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(combo2), "2", "Timer 1 Seconds");
-   gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(combo2), "3", "Timer 2 Seconds");
-   gtk_combo_box_set_active(GTK_COMBO_BOX(combo2), 1);
-   g_signal_connect(combo2, "changed", G_CALLBACK(set_sleep), NULL);
-     
-   label1=gtk_label_new("label1");
-   gtk_widget_set_hexpand(label1, TRUE);
-
-   label2=gtk_label_new("label2");
-   gtk_widget_set_hexpand(label2, TRUE);
-   gtk_widget_set_name (GTK_WIDGET(label2), "css_label2"); 
-
-   event_box1=gtk_event_box_new();
-   gtk_widget_set_hexpand(event_box1, TRUE);
-   gtk_widget_set_vexpand(event_box1, TRUE);
-   gtk_container_add(GTK_CONTAINER(event_box1), label2);
-   gtk_widget_set_events(event_box1, GDK_ENTER_NOTIFY_MASK);
-   gtk_widget_set_events(event_box1, GDK_LEAVE_NOTIFY_MASK);
-   gtk_widget_set_events(event_box1, GDK_BUTTON_PRESS_MASK);
-   g_signal_connect(event_box1, "enter-notify-event", G_CALLBACK(change_font_color_enter_event), GTK_LABEL(label2));
-   g_signal_connect(event_box1, "leave-notify-event", G_CALLBACK(change_font_color_leave_event), GTK_LABEL(label2)); 
-   g_signal_connect(event_box1, "button-press-event", G_CALLBACK(event_box_button_press), GTK_LABEL(label2)); 
-
-   button1=gtk_button_new_with_label("button1");
-   gtk_widget_set_hexpand(button1, TRUE);
-   button_label1=gtk_bin_get_child(GTK_BIN(button1));
-   g_signal_connect(button1, "enter-notify-event", G_CALLBACK(change_font_color_enter), GTK_LABEL(button_label1));
-   g_signal_connect(button1, "leave-notify-event", G_CALLBACK(change_font_color_leave), GTK_LABEL(button_label1));
-
-   button2=gtk_button_new_with_label("button2");
-   gtk_widget_set_hexpand(button2, TRUE);
-   button_label2=gtk_bin_get_child(GTK_BIN(button2));
-   g_signal_connect(button2, "enter-notify-event", G_CALLBACK(change_font_color_enter), GTK_LABEL(button_label2));
-   g_signal_connect(button2, "leave-notify-event", G_CALLBACK(change_font_color_leave), GTK_LABEL(button_label2));
-
-   button3=gtk_button_new_with_label("button3");
-   gtk_widget_set_hexpand(button3, TRUE);
-   button_label3=gtk_bin_get_child(GTK_BIN(button3));
-   g_signal_connect(button3, "enter-notify-event", G_CALLBACK(change_font_color_enter), GTK_LABEL(button_label3));
-   g_signal_connect(button3, "leave-notify-event", G_CALLBACK(change_font_color_leave), GTK_LABEL(button_label3));
-   gtk_widget_set_name(GTK_WIDGET(button3), "css_button3"); 
-
-   button4=gtk_button_new_with_label("button4");
-   gtk_widget_set_hexpand(button4, TRUE);
-   button_label4=gtk_bin_get_child(GTK_BIN(button4));
-   g_signal_connect(button4, "enter-notify-event", G_CALLBACK(change_font_color_enter), GTK_LABEL(button_label4));
-   g_signal_connect(button4, "leave-notify-event", G_CALLBACK(change_font_color_leave), GTK_LABEL(button_label4));
-   gtk_widget_set_name(GTK_WIDGET(button4), "css_button4");    
-
-   drawing_area1=gtk_drawing_area_new();
-   gtk_widget_set_size_request (drawing_area1, 300, 150);
-   g_signal_connect(G_OBJECT(drawing_area1), "draw", G_CALLBACK(select_draw_path), NULL);
-
-   //if(use_thread_code) gtk_widget_set_double_buffered(drawing_area1, FALSE);
-   //if(use_thread_code) g_signal_connect(G_OBJECT(drawing_area1), "draw", G_CALLBACK(draw_radial_color_t1), NULL);
-   //else g_signal_connect(G_OBJECT(drawing_area1), "draw", G_CALLBACK(draw_radial_color), NULL);
-   
-   grid1=gtk_grid_new();
-   gtk_container_add(GTK_CONTAINER(window), grid1);
-
-   gtk_grid_attach(GTK_GRID(grid1), combo1, 0, 0, 1, 1);
-   gtk_grid_attach(GTK_GRID(grid1), combo2, 0, 1, 1, 1);
-   gtk_grid_attach(GTK_GRID(grid1), label1, 0, 2, 1, 1);
-   gtk_grid_attach(GTK_GRID(grid1), event_box1, 0, 3, 1, 1);
-   gtk_grid_attach(GTK_GRID(grid1), button1, 0, 4, 1, 1);
-   gtk_grid_attach(GTK_GRID(grid1), button2, 0, 5, 1, 1);
-   gtk_grid_attach(GTK_GRID(grid1), button3, 0, 6, 1, 1);
-   gtk_grid_attach(GTK_GRID(grid1), button4, 0, 7, 1, 1);
-   gtk_grid_attach(GTK_GRID(grid1), drawing_area1, 0, 8, 3, 1);
-   
-   provider = gtk_css_provider_new();
-   display = gdk_display_get_default();
-   screen = gdk_display_get_default_screen(display);
-   gtk_style_context_add_provider_for_screen(screen, GTK_STYLE_PROVIDER(provider), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
-   gtk_css_provider_load_from_data(provider, css_string, -1, &css_error);
-   if(css_error!=NULL) g_print("CSS loader error %s\n", css_error->message);
-   g_object_unref(provider);
-
-   gtk_widget_show_all(window);
-
-   timer_id=gdk_threads_add_timeout(100, animate_drawing_area, drawing_area1);
-
-   gtk_main();
-   return 0;
-}
