352ffdf4d960e0432156160a4e8dbff84387654d
C. Eric Cashon <cecashon@aol.com>
2014-10-18 15:11:13 -0700

    Add an initialization function to sql_login1.c

--- Misc/sql_login1.c
+++ Misc/sql_login1.c
@@ -14,20 +14,27 @@ C. Eric Cashon
 #include<stdio.h>
 #include<stdbool.h>
 
-static bool check_login_password(char *login, char *password);
-static bool insert_login_password(char *login, char *password);
+static bool initialize_admin_login_password();
+static bool check_login_password(int *admin, char *login, char *password);
+static bool insert_login_password(int admin, char *login, char *password);
 static bool delete_login_password(char *login, char *password);
 static bool update_login_password(char *login_old, char *password_old, char *login_new, char *password_new);
 
 int main()
   {
+    bool table_created=false;
     bool login_exists=false;
+    int admin=0;
 
-    login_exists=check_login_password("Eric", "password1");
-    if(login_exists==true) printf("Found Login\n");
+    table_created=initialize_admin_login_password();
+    if(table_created==true) printf("A New Password Table Created\n");
+    else printf("Password Table Already Exists\n");
+
+    login_exists=check_login_password(&admin, "Eric", "password1");
+    if(login_exists==true) printf("Found Login. Admin %i\n", admin);
     else printf("No Login Found\n");
 
-    login_exists=insert_login_password("Eric", "password1");
+    login_exists=insert_login_password(1, "Eric", "password1");
     if(login_exists==true) printf("Found Existing Login\n");
     else printf("New Login Created\n");
 
@@ -41,52 +48,78 @@ int main()
 
     return 0;
   }
-static bool check_login_password(char *login, char *password)
+//Initialize a default admin and password table. Don't drop table if it already exists.
+static bool initialize_admin_login_password()
   {
-    bool login_exists=false;
     int sql_return=0;
+    bool table_created=false;
     sqlite3 *cnn=NULL;
     sqlite3_stmt *stmt1=NULL;
-    char *sql1="CREATE TABLE IF NOT EXISTS Passwords (login TEXT, password TEXT);";
-    char *sql2=sqlite3_mprintf("SELECT login, password FROM Passwords WHERE login=='%q' AND password=='%q';", login, password);
+    char *sql1="CREATE TABLE IF NOT EXISTS Passwords (admin INTEGER, login TEXT, password TEXT);";
+    char *sql2="SELECT admin FROM Passwords WHERE admin==2;";
+    char *sql3="INSERT INTO Passwords VALUES(2, 'admin', 'password');";
 
-    sqlite3_open("password.db",&cnn);
-    sqlite3_exec(cnn, sql1, 0, 0, 0);   
-    sqlite3_prepare_v2(cnn,sql2,-1,&stmt1,0);
-    sql_return=sqlite3_step(stmt1);
-    if(sql_return==SQLITE_ROW) login_exists=true;
-    sqlite3_finalize(stmt1);   
-    sqlite3_close(cnn); 
-
-    sqlite3_free(sql2);
-    return login_exists;
-  }
-static bool insert_login_password(char *login, char *password)
-  {
-    bool login_exists=false;
-    int sql_return=0;
-    sqlite3 *cnn=NULL;
-    sqlite3_stmt *stmt1=NULL;
-    char *sql1="CREATE TABLE IF NOT EXISTS Passwords (login TEXT, password TEXT);";
-    //check if login and password are already there.
-    char *sql2=sqlite3_mprintf("SELECT login, password FROM Passwords WHERE login=='%q' AND password=='%q';", login, password);
-    char *sql3=sqlite3_mprintf("INSERT INTO Passwords VALUES('%q','%q');", login, password);
-    
     sqlite3_open("password.db",&cnn);
     sqlite3_exec(cnn, sql1, 0, 0, 0);
     sqlite3_prepare_v2(cnn,sql2,-1,&stmt1,0);
+    sql_return=sqlite3_step(stmt1);
+    if(sql_return!=SQLITE_ROW)
+      {
+        table_created=true;
+        sqlite3_exec(cnn, sql3, 0, 0, 0);
+      }
+    sqlite3_finalize(stmt1);  
+    sqlite3_close(cnn);  
+
+    return table_created; 
+  }
+//Check if there is already a login and return the admin status of the login.
+static bool check_login_password(int *admin, char *login, char *password)
+  {
+    bool login_exists=false;
+    int sql_return=0;
+    sqlite3 *cnn=NULL;
+    sqlite3_stmt *stmt1=NULL;
+    char *sql1=sqlite3_mprintf("SELECT admin, login, password FROM Passwords WHERE login=='%q' AND password=='%q';", login, password);
+
+    sqlite3_open("password.db",&cnn);   
+    sqlite3_prepare_v2(cnn,sql1,-1,&stmt1,0);
+    sql_return=sqlite3_step(stmt1);
+    if(sql_return==SQLITE_ROW)
+      {
+         login_exists=true;
+         *admin=sqlite3_column_int(stmt1, 0);
+      }
+    sqlite3_finalize(stmt1);   
+    sqlite3_close(cnn); 
+
+    sqlite3_free(sql1);
+    return login_exists;
+  }
+static bool insert_login_password(int admin, char *login, char *password)
+  {
+    bool login_exists=false;
+    int sql_return=0;
+    sqlite3 *cnn=NULL;
+    sqlite3_stmt *stmt1=NULL;
+    //check if login and password are already there.
+    char *sql1=sqlite3_mprintf("SELECT login, password FROM Passwords WHERE login=='%q' AND password=='%q';", login, password);
+    char *sql2=sqlite3_mprintf("INSERT INTO Passwords VALUES('%i','%q','%q');", admin, login, password);
+    
+    sqlite3_open("password.db",&cnn);
+    sqlite3_prepare_v2(cnn,sql1,-1,&stmt1,0);
     sql_return=sqlite3_step(stmt1);
     if(sql_return==SQLITE_ROW) login_exists=true;
     else
       {
         login_exists=false;
-        sqlite3_exec(cnn, sql3, 0, 0, 0);
+        sqlite3_exec(cnn, sql2, 0, 0, 0);
       }
     sqlite3_finalize(stmt1);     
     sqlite3_close(cnn); 
 
+    sqlite3_free(sql1);
     sqlite3_free(sql2);
-    sqlite3_free(sql3);
 
     return login_exists;
   } 
