8c0ea36b565b94649ef2146fa66ec546a027a974
C. Eric Cashon <cecashon@aol.com>
2014-11-02 16:02:50 -0800

    Test some SVG with drawSVG.py

---
+++ Misc/Python/drawSVG.py
@@ -0,0 +1,138 @@
+#!/user/bin/python
+
+'''
+Test code for looking at Python, GTK+ Cairo and SVG along with some animation.
+From various items from the internet. Just some fun with the technology. The 
+script needs the test1.svg to open.
+'''
+
+from gi.repository import Gtk, GdkPixbuf, Gdk, GObject, cairo
+import time
+import random
+import math
+
+# The number of circles and the window size.
+num = 200
+size = 600
+
+# Initialize circle coordinates and velocities.
+x = []
+y = []
+xv = []
+yv = []
+for i in range(num):
+    x.append(random.randint(0, size))
+    y.append(random.randint(0, size))
+    xv.append(random.randint(-5, 15))
+    yv.append(random.randint(-5, 15))
+
+class DialogMessage(Gtk.MessageDialog):
+    def __init__(self, parent):
+        Gtk.MessageDialog.__init__(self, parent, 0, Gtk.MessageType.INFO,
+            Gtk.ButtonsType.OK, "DateTime")
+        time1 = time.strftime("%a  %b %d, %Y  %-I:%M:%S %p",time.localtime(time.time()))
+        self.format_secondary_text(time1)
+        self.set_default_size(150, 100)
+        self.show_all()
+
+class DialogFileChooser(Gtk.FileChooserDialog):
+    def __init__(self, parent):
+        Gtk.FileChooserDialog.__init__(self, "My Dialog", parent, 0,
+            (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+             Gtk.STOCK_OK, Gtk.ResponseType.OK))
+        self.set_default_size(150, 100)
+        self.show_all()
+
+class MainWindow(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Test SVG")
+        self.set_default_size(500,600);
+        self.connect_after('destroy', self.destroy)
+                
+        box = Gtk.Box(spacing=2)
+        box.set_orientation (Gtk.Orientation.VERTICAL)
+
+        button1 = Gtk.Button ("Open a picture...")
+        button1.connect_after('clicked', self.on_button1_clicked)
+
+        button2 = Gtk.Button ("What time is it?")
+        button2.connect_after('clicked', self.on_button2_clicked)
+
+        self.drawing_area = Gtk.DrawingArea()
+        self.drawing_area.set_size_request(400, 500)
+        self.drawing_area.connect("draw", self.on_draw)
+        self.drawing_area.connect("draw", self.expose)
+
+        box.pack_start (button1, False, False, 0)
+        box.pack_start (button2, False, False, 0)
+        box.pack_start (self.drawing_area, False, False, 0)
+        self.add (box)
+                
+        self.show_all()
+        self.pb = GdkPixbuf.Pixbuf.new_from_file("test1.svg")
+        GObject.idle_add(self.timeout)
+
+    #Text to draw over SVG.
+    def on_draw(self, wid, cr):
+        Gdk.cairo_set_source_pixbuf(cr, self.pb, 5, 5)
+        cr.paint()
+        cr.set_font_size(25)
+        '''
+        Use constants from Cairo documentation for
+        select_font_face("Serif", enum cairo_font_slant_t, enum cairo_font_weight_t)
+        http://cairographics.org/manual/cairo-text.html
+        ''' 
+        cr.select_font_face("Serif", 0, 1)
+        cr.set_source_rgb(0.9 , 0.9 , 0.0)
+        cr.move_to(70 , 450)
+        cr.rotate(-3.14/6)
+        cr.show_text("Cairo Text Overlay of SVG Image")
+        cr.stroke() 
+
+    # Draw the circles and update their positions.
+    def expose(self, wid, cr):
+        cr.set_line_width(4)
+        for i in range(num):
+            cr.set_source_rgb(0, 1, 0)
+            cr.translate(-7, 0)
+            cr.arc(x[i], y[i], 8, 0, 2 * math.pi)
+            cr.stroke_preserve()
+            cr.set_source_rgb(1, 1, 1)
+            cr.fill()
+            x[i] += xv[i]
+            y[i] += yv[i]
+            if x[i] > size or x[i] < 0:
+                xv[i] = -xv[i]
+            if y[i] > size or y[i] < 0:
+                yv[i] = -yv[i]
+
+    def timeout(self):
+        self.drawing_area.queue_draw()
+        return True
+
+    def on_button1_clicked(self, widget):
+        dialog = DialogFileChooser(self)
+        response = dialog.run()
+
+        if response == Gtk.ResponseType.OK:
+            print("The OK button was clicked")
+            self.pb = GdkPixbuf.Pixbuf.new_from_file(dialog.get_filename())
+        elif response == Gtk.ResponseType.CANCEL:
+            print("The Cancel button was clicked")
+        dialog.destroy()
+
+    def on_button2_clicked(self, widget):
+        dialog2 = DialogMessage(self)
+        dialog2.run()
+        dialog2.destroy()
+
+    def destroy(window, self):
+        Gtk.main_quit()
+
+def main():    
+    app = MainWindow()
+    Gtk.main()
+        
+if __name__ == "__main__":    
+    main()
+
