a8f93ed2a539bff3c134e2c8d962216d2035af8c
C. Eric Cashon <cecashon@aol.com>
2014-11-07 16:09:26 -0800

    Add a column property to popup_tree1.py

--- Misc/Python/popup_tree1.py
+++ Misc/Python/popup_tree1.py
@@ -13,6 +13,7 @@ class CellRendererTextWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="Treeview Popup")
         self.set_border_width(10)
+        self.set_position(Gtk.WindowPosition.CENTER)
         self.set_default_size(250, 100)
         self.x1=0
         self.y1=0
@@ -36,23 +37,24 @@ class CellRendererTextWindow(Gtk.Window):
         self.treeview.connect("button-press-event", self.show_menu)
 
         self.renderer_text1 = Gtk.CellRendererText()
-        self.column_text1 = Gtk.TreeViewColumn("Text1", self.renderer_text1, text=0)
+        self.renderer_text1.set_property("background", "red")
+        self.column_text1 = Gtk.TreeViewColumn("Title1", self.renderer_text1, text=0)
         self.renderer_text1.set_property("editable", True)    
         self.treeview.append_column(self.column_text1)
         self.renderer_text1.connect("edited", self.text_edited1)
 
         self.renderer_text2 = Gtk.CellRendererText()
         self.renderer_text2.set_property("editable", True)        
-        self.column_text2 = Gtk.TreeViewColumn("Text2", self.renderer_text2, text=1)
+        self.column_text2 = Gtk.TreeViewColumn("Title2", self.renderer_text2, text=1)
         self.treeview.append_column(self.column_text2)
         self.renderer_text2.connect("edited", self.text_edited2)
 
         style_provider = Gtk.CssProvider()
         css = """GtkWindow{background: blue;}
-                 GtkTreeView{background: yellow;}
+                 GtkTreeView{color: black}
                  GtkTreeView:selected{color: black; background: green; border-width: 1px; border-color: black;}
                  GtkMenu#css_popup1{color: black; background: green;}
-                 column-header .button{color: white; background: red;}"""
+                 column-header .button{color: white; background: purple;}"""
         style_provider.load_from_data(css)
         Gtk.StyleContext.add_provider_for_screen(Gdk.Screen.get_default(), style_provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
 
