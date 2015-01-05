21dcfd5e65d79d06575a1fd94bb83956249b9c30
C. Eric Cashon <cecashon@aol.com>
2015-01-05 13:45:23 -0800

    Add menu to gtk_app1.c

--- Misc/Csamples/gtk_app1.c
+++ Misc/Csamples/gtk_app1.c
@@ -11,7 +11,46 @@ The code needs a png file in order to work. Change the icon_test.png to a valid 
 
 #include<gtk/gtk.h>
 
-static void activate(GtkApplication *app, gpointer user_data)
+static void quit_program(GSimpleAction *action, GVariant *parameter, gpointer user_data)
+ {
+   g_application_quit(G_APPLICATION(user_data));
+ }
+static void test1_callback(GSimpleAction *action, GVariant *parameter, gpointer user_data)
+ {
+   g_print("Test1 Callback\n");
+ }
+static void app_startup(GtkApplication *app, gpointer user_data)
+ {
+   //Setup menu callbacks.
+   GSimpleAction *test1;
+   GSimpleAction *quit;
+   test1 = g_simple_action_new("test1", NULL);
+   g_signal_connect(test1, "activate", G_CALLBACK(test1_callback), app);
+   g_action_map_add_action(G_ACTION_MAP(app), G_ACTION(test1));
+   g_object_unref(test1);
+   quit = g_simple_action_new("quit", NULL);
+   g_signal_connect(quit, "activate", G_CALLBACK(quit_program), app);
+   g_action_map_add_action(G_ACTION_MAP(app), G_ACTION(quit));
+   g_object_unref(quit);
+
+   //Setup menu.
+   GMenu *menu;
+   GMenu *submenu;  
+   GMenu *section;  
+   menu = g_menu_new();
+   submenu = g_menu_new();
+   g_menu_append_submenu(menu, "Application", G_MENU_MODEL(submenu));
+   section = g_menu_new();
+   g_menu_append_section(submenu, NULL, G_MENU_MODEL(section));
+   g_menu_append(section, "Test1", "app.test1");
+   g_menu_append(section, "Quit", "app.quit");
+   g_object_unref(submenu);
+   g_object_unref(section);
+
+   gtk_application_set_menubar(GTK_APPLICATION(app), G_MENU_MODEL(menu));
+   g_object_unref(menu);
+ }
+static void app_activate(GtkApplication *app, gpointer user_data)
  {
    GtkWidget *window, *label1, *grid;
    GError *error = NULL;
@@ -20,8 +59,9 @@ static void activate(GtkApplication *app, gpointer user_data)
    gtk_window_set_title(GTK_WINDOW(window), "Application Test");
    gtk_window_set_default_size(GTK_WINDOW(window), 250, 100);
    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+   gtk_container_set_border_width(GTK_CONTAINER(window), 20);
    //icon gets placed on the launcher. Need to supply a valid png in the working folder.
-   GdkPixbuf *pixbuf=gdk_pixbuf_new_from_file("icon_test.png", &error);
+   GdkPixbuf *pixbuf=gdk_pixbuf_new_from_file("dino.svg", &error);
    if(!pixbuf)
      {
        g_print("%s\n", error->message);
@@ -31,14 +71,13 @@ static void activate(GtkApplication *app, gpointer user_data)
      {
        gtk_window_set_icon(GTK_WINDOW(window), pixbuf);
      }
-   gtk_container_set_border_width(GTK_CONTAINER(window), 20);
 
-   label1=gtk_label_new("GTK+ Application with Icon\n in Launcher");
+   label1=gtk_label_new("GMenu with icon");
    gtk_widget_set_hexpand(label1, TRUE);
 
    grid=gtk_grid_new();
    gtk_grid_set_row_spacing(GTK_GRID(grid), 10);
-   gtk_grid_attach(GTK_GRID(grid), label1, 0, 0, 1, 1);
+   gtk_grid_attach(GTK_GRID(grid), label1, 0, 1, 1, 1);
    
    gtk_container_add(GTK_CONTAINER(window), grid);
 
@@ -49,7 +88,8 @@ int main(int argc, char **argv)
    GtkApplication *app;
    int status;
    app = gtk_application_new("app.example", G_APPLICATION_FLAGS_NONE);
-   g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+   g_signal_connect(app, "startup", G_CALLBACK(app_startup), NULL);
+   g_signal_connect(app, "activate", G_CALLBACK(app_activate), NULL);
    status=g_application_run(G_APPLICATION(app), argc, argv);
    g_object_unref(app);
    return(status);
