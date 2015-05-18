c5d7cedfeebfdf7814c1756f47a5c19a6f261396
C. Eric Cashon <cecashon@aol.com>
2015-05-18 15:06:13 -0700

    Test resize with drawSVG.py

--- Misc/Python/drawSVG.py
+++ Misc/Python/drawSVG.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/user/bin/python
 
 '''
    Test code for looking at Python2.7, GTK+ Cairo, CSS and SVG along with some animation.
@@ -17,20 +17,21 @@ import time
 import random
 import math
 
-# The number of circles and the window size.
-num = 200
-size = 600
-
 # Initialize circle coordinates and velocities.
-x = []
-y = []
-xv = []
-yv = []
-for i in range(num):
-    x.append(random.randint(0, size))
-    y.append(random.randint(0, size))
-    xv.append(random.randint(-5, 15))
-    yv.append(random.randint(-5, 15))
+class InitializeCircles:
+    def __init__(self):
+        self.num = 100
+        self.size_x = 800
+        self.size_y = 600
+        self.x = []
+        self.y = []
+        self.xv = []
+        self.yv = []
+        for i in range(self.num):
+            self.x.append(random.randint(0, self.size_x))
+            self.y.append(random.randint(0, self.size_y))
+            self.xv.append(random.randint(-5, 15))
+            self.yv.append(random.randint(-5, 15))
 
 class DialogTime(Gtk.MessageDialog):
     def __init__(self, parent):
@@ -60,30 +61,34 @@ class DialogFileChooser(Gtk.FileChooserDialog):
 class MainWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="Test SVG")
-        self.set_default_size(500,600);
+        self.set_default_size(300,350);
         self.set_position(Gtk.WindowPosition.CENTER)
         self.connect_after('destroy', self.destroy)
-                
-        box = Gtk.Box(spacing=2)
-        box.set_orientation (Gtk.Orientation.VERTICAL)
+         
+        self.circles = InitializeCircles()
 
         button1 = Gtk.Button ("Open a picture...")
+        button1.set_hexpand(True)
         button1.set_name("css_button1")
         button1.connect_after('clicked', self.on_button1_clicked)
 
         button2 = Gtk.Button ("What time is it?")
+        button2.set_hexpand(True)
         button2.set_name("css_button2")
         button2.connect_after('clicked', self.on_button2_clicked)
 
         self.drawing_area = Gtk.DrawingArea()
-        self.drawing_area.set_size_request(400, 500)
+        self.drawing_area.set_hexpand(True)
+        self.drawing_area.set_vexpand(True)
+        self.drawing_area.set_size_request(300, 300)
         self.drawing_area.connect("draw", self.on_draw)
         self.drawing_area.connect("draw", self.expose)
 
-        box.pack_start (button1, False, False, 0)
-        box.pack_start (button2, False, False, 0)
-        box.pack_start (self.drawing_area, False, False, 0)
-        self.add (box)
+        self.grid = Gtk.Grid()
+        self.grid.attach(button1, 0, 0, 1, 1)
+        self.grid.attach(button2, 0, 1, 1, 1)
+        self.grid.attach(self.drawing_area, 0, 2, 1, 1)
+        self.add(self.grid)
 
         #Add the CSS linear gradients.
         style_provider = Gtk.CssProvider()
@@ -95,7 +100,7 @@ class MainWindow(Gtk.Window):
                 
         self.show_all()
         try:
-            self.pb = GdkPixbuf.Pixbuf.new_from_file("test1.svg")
+            self.pb = GdkPixbuf.Pixbuf.new_from_file_at_scale("test1.svg", 300, 300, False)
         except:
             dialog = DialogMessage(self, "Couldn't find test1.svg.")
             dialog.run()
@@ -104,7 +109,10 @@ class MainWindow(Gtk.Window):
 
     #Text to draw over SVG.
     def on_draw(self, wid, cr):
-        Gdk.cairo_set_source_pixbuf(cr, self.pb, 5, 5)
+        width = self.drawing_area.get_allocated_width()
+        height = self.drawing_area.get_allocated_height()
+        pixbuf = self.pb.scale_simple(width, height, 2)
+        Gdk.cairo_set_source_pixbuf(cr, pixbuf, 5, 5)
         cr.paint()
         cr.set_font_size(25)
         '''
@@ -114,27 +122,27 @@ class MainWindow(Gtk.Window):
         ''' 
         cr.select_font_face("Serif", 0, 1)
         cr.set_source_rgb(0.9 , 0.9 , 0.0)
-        cr.move_to(70 , 450)
+        cr.move_to(50 , 250)
         cr.rotate(-3.14/6)
-        cr.show_text("Cairo Text Overlay of SVG Image")
+        cr.show_text("Cairo Text Over SVG")
         cr.stroke() 
 
     # Draw the circles and update their positions.
     def expose(self, wid, cr):
         cr.set_line_width(4)
-        for i in range(num):
+        for i in range(self.circles.num):
             cr.set_source_rgb(0, 1, 0)
             cr.translate(-7, 0)
-            cr.arc(x[i], y[i], 8, 0, 2 * math.pi)
+            cr.arc(self.circles.x[i], self.circles.y[i], 8, 0, 2 * math.pi)
             cr.stroke_preserve()
             cr.set_source_rgb(1, 1, 1)
             cr.fill()
-            x[i] += xv[i]
-            y[i] += yv[i]
-            if x[i] > size or x[i] < 0:
-                xv[i] = -xv[i]
-            if y[i] > size or y[i] < 0:
-                yv[i] = -yv[i]
+            self.circles.x[i] += self.circles.xv[i]
+            self.circles.y[i] += self.circles.yv[i]
+            if self.circles.x[i] > self.circles.size_x or self.circles.x[i] < 0:
+                self.circles.xv[i] = -self.circles.xv[i]
+            if self.circles.y[i] > self.circles.size_y or self.circles.y[i] < 0:
+                self.circles.yv[i] = -self.circles.yv[i]
 
     def timeout(self):
         self.drawing_area.queue_draw()
