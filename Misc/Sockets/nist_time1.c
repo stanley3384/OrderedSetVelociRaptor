94719cb646a03f5f2210e36f466ad61819030c9a
C. Eric Cashon <cecashon@aol.com>
2015-12-09 12:05:08 -0800

    Add electron icon to nist_time1.c

--- Misc/Sockets/nist_time1.c
+++ Misc/Sockets/nist_time1.c
@@ -26,17 +26,31 @@ static void nist_atomic_time(GtkWidget *widget, gpointer *data);
 //http://www.ibm.com/developerworks/aix/library/au-endianc/
 unsigned long int unpacku32_lendian(unsigned char *buffer);
 int check_endianess();
+static GdkPixbuf* draw_icon();
+static void about_dialog(GtkWidget *widget, gpointer data);
 
 int main(int argc, char *argv[])
   {   
     gtk_init(&argc, &argv);
   
     GtkWidget *window=gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "NIST Time");
+    gtk_window_set_title(GTK_WINDOW(window), "NIST Atomic Time");
     gtk_container_set_border_width(GTK_CONTAINER(window),10);
     gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
     gtk_window_set_default_size(GTK_WINDOW(window), 700, 300);
     g_signal_connect(window,"destroy",G_CALLBACK(gtk_main_quit), NULL);
+
+    GdkPixbuf *icon=draw_icon();
+    gtk_window_set_default_icon(icon);
+
+    GtkWidget *menu1=gtk_menu_new();
+    GtkWidget *menu1item1=gtk_menu_item_new_with_label("NIST Atomic Time");
+    gtk_menu_shell_append(GTK_MENU_SHELL(menu1), menu1item1);
+    GtkWidget *title1=gtk_menu_item_new_with_label("About");
+    gtk_menu_item_set_submenu(GTK_MENU_ITEM(title1), menu1);
+    GtkWidget *menu_bar=gtk_menu_bar_new();
+    gtk_menu_shell_append(GTK_MENU_SHELL(menu_bar), title1);
+    g_signal_connect(menu1item1, "activate", G_CALLBACK(about_dialog), window);
 
     GtkWidget *radio1=gtk_radio_button_new_with_label(NULL, "NTP Time");
     GtkWidget *radio2=gtk_radio_button_new_with_label_from_widget(GTK_RADIO_BUTTON(radio1), "Time protocol");
@@ -58,6 +72,7 @@ int main(int argc, char *argv[])
     gtk_grid_attach(GTK_GRID(grid), radio2, 2, 1, 1, 1);
     gtk_grid_attach(GTK_GRID(grid), button1, 0, 2, 4, 1);
     gtk_grid_attach(GTK_GRID(grid), label1, 0, 3, 4, 1);
+    gtk_grid_attach(GTK_GRID(grid), menu_bar, 0, 4, 1, 1);
     gtk_container_add(GTK_CONTAINER(window), grid);
 
     GError *css_error=NULL;
@@ -293,6 +308,82 @@ int check_endianess()
     if(p[0]==1) return LITTLE_ENDIAN1;
     else return BIG_ENDIAN1;
   }
+static GdkPixbuf* draw_icon()
+  {
+    //Create a surface to draw a 256x256 icon. 
+    cairo_surface_t *surface=cairo_image_surface_create(CAIRO_FORMAT_ARGB32, 256, 256);
+    cairo_t *cr=cairo_create(surface);
+    gint i=0;
+    
+    //Paint the background.
+    cairo_set_source_rgb(cr, 0.0, 0.0, 0.4);
+    cairo_paint(cr);
+
+    //A blue icon border.
+    cairo_set_source_rgb(cr, 0.0, 0.0, 1.0);
+    cairo_set_line_width(cr, 6);
+    cairo_rectangle(cr, 0, 0, 256, 256);
+    cairo_stroke(cr);
+
+    cairo_pattern_t *pattern1=cairo_pattern_create_linear(0.0, 0.0,  350.0, 0.0);
+    for(i=0;i<=200;i+=20)
+      { 
+        cairo_pattern_add_color_stop_rgb(pattern1, (double)(i/200.0), 1.0, 1.0, 0.0); 
+        cairo_pattern_add_color_stop_rgb(pattern1, (double)(i+10.0)/200.0, 1.0, 0.0, 0.0); 
+      }
+ 
+    //The wave.    
+    cairo_save(cr);
+    cairo_set_source(cr, pattern1);
+    cairo_set_line_width(cr, 3);
+    cairo_move_to(cr, 0, 64);
+    cairo_curve_to(cr, 20, 110, 180, 120, 190, 128);
+    cairo_stroke_preserve(cr);
+    cairo_curve_to(cr, 180, 134, 20, 150, 0, 196);
+    cairo_stroke_preserve(cr);
+    cairo_close_path(cr);
+    cairo_stroke_preserve(cr);
+    cairo_fill(cr);
+    cairo_restore(cr);
+
+    //The electron.
+    cairo_save(cr);
+    cairo_set_source_rgb(cr, 0.6, 0.6, 1.0);
+    cairo_scale(cr, 1.0, 0.5);
+    cairo_arc(cr, 220, 256, 25, 0, 2*G_PI);
+    cairo_fill(cr);
+    cairo_stroke(cr);
+    cairo_restore(cr);
+
+    //The electron line.
+    cairo_set_source_rgb(cr, 0.0, 0.0, 0.0);
+    cairo_set_line_width(cr, 2);
+    cairo_move_to(cr, 210, 128);
+    cairo_line_to(cr, 230, 128);
+    cairo_stroke(cr);
+
+    GdkPixbuf *icon=gdk_pixbuf_get_from_surface(surface, 0, 0, 256, 256);
+
+    cairo_pattern_destroy(pattern1);
+    cairo_destroy(cr);
+    cairo_surface_destroy(surface); 
+    return icon;
+  }
+static void about_dialog(GtkWidget *widget, gpointer data)
+  {
+    GtkWidget *dialog=gtk_about_dialog_new();
+    //Null will add the program icon to the logo.
+    gtk_about_dialog_set_logo(GTK_ABOUT_DIALOG(dialog), NULL);
+    gtk_about_dialog_set_program_name(GTK_ABOUT_DIALOG(dialog), "NIST Atomic Time");
+    gtk_about_dialog_set_version(GTK_ABOUT_DIALOG(dialog), "Test Version 1.0");
+    gtk_about_dialog_set_comments(GTK_ABOUT_DIALOG(dialog), "Compare system time with NIST time.");
+   
+    gtk_widget_show_all(dialog);
+    gtk_dialog_run(GTK_DIALOG(dialog));
+    gtk_widget_destroy(dialog);
+  }
+
+
 
 
 
