142cdb5cb64c10daac8e75bf9d0a0a3f9e789030
eric <cecashon@aol.com>
2016-12-09 11:13:22 -0800

    Just increment needle values in adjustable_gauges.c

--- Misc/AdjustableGauge/adjustable_gauges.c
+++ Misc/AdjustableGauge/adjustable_gauges.c
@@ -1,7 +1,7 @@
 
 /*
 
-    Another test program for the adjustable gauge widget. Test four gauges with some random numbers.
+    Another test program for the adjustable gauge widget. Test four gauges with some numbers.
 
     gcc -Wall -Werror adjustable_gauge.c adjustable_gauges.c -o gauges `pkg-config --cflags --libs gtk+-3.0` -lm
 
@@ -14,15 +14,22 @@
 #include <gtk/gtk.h>
 #include "adjustable_gauge.h"
 
-static GRand *rand;
+//Save the needle position between calls.
+static gdouble needle1=0.0;
+static gdouble needle2=0.0;
+static gdouble needle3=20.0;
+static gdouble needle4=0.0;
+//The top of each scale.
+static const gdouble top1=100.0;
+static const gdouble top2=200.0;
+static const gdouble top3=200.0;
+static const gdouble top4=500.0;
 
 static gboolean time_draw(GtkWidget *widgets[]);
 
 int main(int argc, char **argv)
   {      
     gtk_init(&argc, &argv);   
-
-    rand=g_rand_new();
 
     GtkWidget *window=gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
@@ -43,7 +50,7 @@ int main(int argc, char **argv)
     gtk_widget_set_vexpand(gauge2, TRUE);
     //Set scale values before cutoff values.
     adjustable_gauge_set_scale_bottom(ADJUSTABLE_GAUGE(gauge2), 0.0);
-    adjustable_gauge_set_scale_top(ADJUSTABLE_GAUGE(gauge2), 200.0);
+    adjustable_gauge_set_scale_top(ADJUSTABLE_GAUGE(gauge2), top2);
     adjustable_gauge_set_first_cutoff(ADJUSTABLE_GAUGE(gauge2), 150.0);
     adjustable_gauge_set_second_cutoff(ADJUSTABLE_GAUGE(gauge2), 180.0); 
   
@@ -51,7 +58,7 @@ int main(int argc, char **argv)
     gtk_widget_set_hexpand(gauge3, TRUE);
     gtk_widget_set_vexpand(gauge3, TRUE);
     adjustable_gauge_set_scale_bottom(ADJUSTABLE_GAUGE(gauge3), 20.0);
-    adjustable_gauge_set_scale_top(ADJUSTABLE_GAUGE(gauge3), 200.0);
+    adjustable_gauge_set_scale_top(ADJUSTABLE_GAUGE(gauge3), top3);
     adjustable_gauge_set_first_cutoff(ADJUSTABLE_GAUGE(gauge3), 120.0);
     adjustable_gauge_set_second_cutoff(ADJUSTABLE_GAUGE(gauge3), 170.0); 
    
@@ -59,7 +66,7 @@ int main(int argc, char **argv)
     gtk_widget_set_hexpand(gauge4, TRUE);
     gtk_widget_set_vexpand(gauge4, TRUE);
     adjustable_gauge_set_scale_bottom(ADJUSTABLE_GAUGE(gauge4), 0.0);
-    adjustable_gauge_set_scale_top(ADJUSTABLE_GAUGE(gauge4), 500.0);
+    adjustable_gauge_set_scale_top(ADJUSTABLE_GAUGE(gauge4), top4);
     adjustable_gauge_set_first_cutoff(ADJUSTABLE_GAUGE(gauge4), 450.0);
     adjustable_gauge_set_second_cutoff(ADJUSTABLE_GAUGE(gauge4), 480.0); 
     
@@ -72,42 +79,72 @@ int main(int argc, char **argv)
     gtk_container_add(GTK_CONTAINER(window), grid);
   
     GtkWidget *widgets[]={gauge1, gauge2, gauge3, gauge4};    
-    g_timeout_add(1000, (GSourceFunc)time_draw, widgets);
+    g_timeout_add(100, (GSourceFunc)time_draw, widgets);
 
     gtk_widget_show_all(window);                  
     gtk_main();
-
-    g_rand_free(rand);
 
     return 0;
   }
 static gboolean time_draw(GtkWidget *widgets[])
   {
     gint i=0;
-    gdouble needle=0;
+
+    needle1++;
+    needle2++;
+    needle3++;
+    needle4++;
     for(i=0;i<4;i++)
       {
         switch(i)
           {
             case 0:
-              needle=100*g_rand_double(rand);
-              //g_print("Needle1 %f\n", needle);
-              adjustable_gauge_set_needle(ADJUSTABLE_GAUGE(widgets[i]), needle);
+              if(needle1<top1)
+                {
+                  adjustable_gauge_set_needle(ADJUSTABLE_GAUGE(widgets[i]), needle1);
+                }
+              else
+                {
+                  g_print("Reset Gauge1\n");
+                  needle1=0.0;
+                  adjustable_gauge_set_needle(ADJUSTABLE_GAUGE(widgets[i]), needle1);
+                }
               break;
             case 1:
-              needle=200*g_rand_double(rand);
-              //g_print("Needle2 %f\n", needle);
-              adjustable_gauge_set_needle(ADJUSTABLE_GAUGE(widgets[i]), needle);
+              if(needle2<top2)
+                {
+                  adjustable_gauge_set_needle(ADJUSTABLE_GAUGE(widgets[i]), needle2);
+                }
+              else
+                {
+                  g_print("Reset Gauge2\n");
+                  needle2=0.0;
+                  adjustable_gauge_set_needle(ADJUSTABLE_GAUGE(widgets[i]), needle2);
+                }
               break;
             case 2:
-              needle=180*g_rand_double(rand)+20;
-              //g_print("Needle3 %f\n", needle);
-              adjustable_gauge_set_needle(ADJUSTABLE_GAUGE(widgets[i]), needle);
+              if(needle3<top3)
+                {
+                  adjustable_gauge_set_needle(ADJUSTABLE_GAUGE(widgets[i]), needle3);
+                }
+              else
+                {
+                  g_print("Reset Gauge3\n");
+                  needle3=20.0;
+                  adjustable_gauge_set_needle(ADJUSTABLE_GAUGE(widgets[i]), needle3);
+                }
               break;
             case 3:
-              needle=500*g_rand_double(rand);
-              //g_print("Needle4 %f\n", needle);
-              adjustable_gauge_set_needle(ADJUSTABLE_GAUGE(widgets[i]), needle);
+              if(needle4<top4)
+                {
+                  adjustable_gauge_set_needle(ADJUSTABLE_GAUGE(widgets[i]), needle4);
+                }
+              else
+                {
+                  g_print("Reset Gauge4\n");
+                  needle4=0.0;
+                  adjustable_gauge_set_needle(ADJUSTABLE_GAUGE(widgets[i]), needle4);
+                }
           }        
       }
     
