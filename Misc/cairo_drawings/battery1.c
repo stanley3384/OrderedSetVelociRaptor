75082d8388081203b98568afede72c81f116827d
eric <cecashon@aol.com>
2017-01-13 11:24:57 -0800

    Add a percentage count for battery1.c

--- Misc/cairo_drawings/battery1.c
+++ Misc/cairo_drawings/battery1.c
@@ -65,14 +65,15 @@ static void button_clicked(GtkWidget *button, GtkWidget *widgets[])
  {
    animate=9.0;
    charging=TRUE;
+   gtk_widget_queue_draw(widgets[1]);
    gtk_widget_set_sensitive(button, FALSE);
-   g_timeout_add(200, (GSourceFunc)draw_battery, widgets);
+   g_timeout_add(100, (GSourceFunc)draw_battery, widgets);
  }
 static gboolean draw_battery(GtkWidget *widgets[])
  {
    if(animate>1.0)
      {
-       animate-=0.25;
+       animate-=0.05;
        gtk_widget_queue_draw(widgets[1]);
        return TRUE;
      }
@@ -106,6 +107,12 @@ static gboolean da_drawing(GtkWidget *da, cairo_t *cr, gpointer data)
    if(translate==1) cairo_translate(cr, width/4.0, 0.0);
    if(translate==2) cairo_translate(cr, width/3.0, 0.0);
 
+   //Battery background rectangle.
+   cairo_set_source_rgba(cr, 0.0, 0.0, 0.0, 1.0);
+   cairo_rectangle(cr, 3.0*width/10.0, height/10.0, 4.0*width/10.0, 8.0*height/10.0);
+   cairo_fill(cr);
+
+   //The red, yellow to green gradient.
    cairo_pattern_t *pattern1=cairo_pattern_create_linear(4.0*width/10.0, height/10.0, 4.0*width/10.0, 8.0*height/10.0); 
    cairo_pattern_add_color_stop_rgba(pattern1, 0.0, 0.0, 1.0, 0.0, 1.0); 
    cairo_pattern_add_color_stop_rgba(pattern1, 0.5, 1.0, 1.0, 0.0, 1.0);
@@ -114,11 +121,13 @@ static gboolean da_drawing(GtkWidget *da, cairo_t *cr, gpointer data)
    cairo_rectangle(cr, 3.0*width/10.0, animate*height/10.0, 4.0*width/10.0, (9.0-animate)*height/10.0);
    cairo_fill(cr);
 
+   //Battery outline rectangle.
    cairo_set_line_width(cr, 8.0);
    cairo_set_source_rgba(cr, 0.8, 0.8, 0.8, 1.0);
    cairo_rectangle(cr, 3.0*width/10.0, height/10.0, 4.0*width/10.0, 8.0*height/10.0);
    cairo_stroke(cr);
 
+   //Battery top.
    cairo_rectangle(cr, 4.5*width/10.0, height/20.0, 1.0*width/10.0, height/20.0);
    cairo_fill(cr);
 
@@ -128,13 +137,13 @@ static gboolean da_drawing(GtkWidget *da, cairo_t *cr, gpointer data)
        cairo_set_line_width(cr, 1.0);
        cairo_set_source_rgba(cr, 0.0, 1.0, 1.0, 1.0);
        cairo_move_to(cr, 6.0*width/10.0, 2.0*height/10.0);
-       cairo_line_to(cr, 4.0*width/10.0, 5.25*height/10.0);
+       cairo_line_to(cr, 4.25*width/10.0, 5.25*height/10.0);
        cairo_stroke_preserve(cr);
        cairo_line_to(cr, 5.0*width/10.0, 5.25*height/10.0);
        cairo_stroke_preserve(cr);
        cairo_line_to(cr, 4.0*width/10.0, 8.0*height/10.0);
        cairo_stroke_preserve(cr);
-       cairo_line_to(cr, 6.0*width/10.0, 4.75*height/10.0);
+       cairo_line_to(cr, 5.75*width/10.0, 4.75*height/10.0);
        cairo_stroke_preserve(cr);
        cairo_line_to(cr, 5.0*width/10.0, 4.75*height/10.0);
        cairo_stroke_preserve(cr);
@@ -142,6 +151,17 @@ static gboolean da_drawing(GtkWidget *da, cairo_t *cr, gpointer data)
        cairo_fill(cr);
        cairo_stroke(cr);
      }
+
+   //Add percentage text.
+   cairo_text_extents_t extents1;
+   cairo_set_source_rgba(cr, 0.0, 1.0, 1.0, 1.0);
+   cairo_select_font_face(cr, "Arial", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_BOLD);
+   cairo_set_font_size(cr, 16);
+   gchar *percent=g_strdup_printf("%.2f", ((9.0-animate)/8.0)*100.0);
+   cairo_text_extents(cr, percent, &extents1);
+   cairo_move_to(cr, width/2.0 - extents1.width/2, 9.5*height/10.0 + extents1.height/2);  
+   cairo_show_text(cr, percent);
+   g_free(percent);
 
    if(translate==1) cairo_translate(cr, -width/4.0, 0.0);
    if(translate==2) cairo_translate(cr, -width/3.0, 0.0);
