1b4461a335fe3b621d2850711070c3de06eeeed3
eric <cecashon@aol.com>
2017-01-21 12:54:36 -0800

    Add tags to search_textbuffer1.c

--- Misc/Csamples/search_textbuffer1.c
+++ Misc/Csamples/search_textbuffer1.c
@@ -13,6 +13,9 @@
 static void button_clicked(GtkWidget *button, gpointer *data)
   {
     GtkTextIter start;
+    GtkTextIter end;
+    GtkTextIter start_word;
+    GtkTextIter end_word;
     gchar *search_string=g_strdup(gtk_entry_get_text(GTK_ENTRY(data[1])));
     gchar *p=search_string;
     glong count=g_utf8_strlen(search_string, -1);
@@ -24,17 +27,25 @@ static void button_clicked(GtkWidget *button, gpointer *data)
     if(count>0)
       {
         gtk_text_buffer_get_start_iter(GTK_TEXT_BUFFER(data[0]), &start);
+        gtk_text_buffer_get_end_iter(GTK_TEXT_BUFFER(data[0]), &end);
+        gtk_text_buffer_remove_all_tags(GTK_TEXT_BUFFER(data[0]), &start, &end);
+        start_word=start;
         do
           {
             if(g_unichar_toupper(g_utf8_get_char(p))==gtk_text_iter_get_char(&start)||g_unichar_tolower(g_utf8_get_char(p))==gtk_text_iter_get_char(&start))
               {
+                end_word=start;
+                gtk_text_iter_forward_char(&end_word);
                 *temp_p=gtk_text_iter_get_char(&start);
                 counter++;
                 temp_p=g_utf8_offset_to_pointer(temp_string, counter); 
                 p=g_utf8_find_next_char(p, NULL); //p++
                 if(counter>=count)
                   {
+                    if(!gtk_text_iter_is_start(&start_word)) gtk_text_iter_forward_char(&start_word);
+                    gtk_text_buffer_apply_tag_by_name(GTK_TEXT_BUFFER(data[0]), "yellow-tag", &start_word, &end_word);
                     g_print("%s\n", temp_string);
+                    start_word=start;
                     counter=0;
                     p=search_string;
                     temp_p=temp_string;
@@ -48,6 +59,7 @@ static void button_clicked(GtkWidget *button, gpointer *data)
                     p=search_string;
                     temp_p=temp_string;
                   }
+                start_word=start;
               }
           }while(gtk_text_iter_forward_char(&start));
       }    
@@ -60,7 +72,7 @@ int main(int argc, char *argv[])
     gtk_init(&argc, &argv);
 
     GtkWidget *window=gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Textview");
+    gtk_window_set_title(GTK_WINDOW(window), "Search Textbuffer");
     gtk_window_set_default_size(GTK_WINDOW(window), 400, 400);
     gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
     gtk_container_set_border_width(GTK_CONTAINER(window), 20);
@@ -69,8 +81,10 @@ int main(int argc, char *argv[])
     GtkWidget *textview=gtk_text_view_new();
     gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(textview), GTK_WRAP_WORD);
     gtk_widget_set_size_request(textview, 400, 300);
+
     GtkTextBuffer *buffer=gtk_text_view_get_buffer(GTK_TEXT_VIEW(textview));
     gtk_text_buffer_set_text(buffer, "SEArch Add some words to Search search SearCH SEaRch search search Search and a few extra s ss sssr Sea Sear.", -1);
+    gtk_text_buffer_create_tag(buffer, "yellow-tag", "background", "yellow", NULL); 
 
     GtkWidget *entry=gtk_entry_new();
     gtk_entry_set_text(GTK_ENTRY(entry), "search");
