419f28ce367a82d4ca7377441dfb4900b0f5ed79
eric <cecashon@aol.com>
2017-02-24 14:00:57 -0800

    Add clock to circle_gradient1.c

--- Misc/cairo_drawings/circle_gradient1.c
+++ Misc/cairo_drawings/circle_gradient1.c
@@ -1,9 +1,9 @@
 
 /*
     Try drawing a circular gradient with cairo and a couple of tensor-product patch meshes.
-Draw four arc patches of a circle.
+Draw four arc patches of a circle. Then add the rest to make a clock.
    
-    gcc -Wall circle_gradient1.c -o circle_gradient1 `pkg-config --cflags --libs gtk+-3.0`
+    gcc -Wall circle_gradient1.c -o circle_gradient1 `pkg-config --cflags --libs gtk+-3.0` -lm
 
     Tested on Ubuntu16.04 and GTK3.18
 
@@ -11,7 +11,9 @@ Draw four arc patches of a circle.
 */
 
 #include<gtk/gtk.h>
+#include<math.h>
 
+static gboolean time_redraw(gpointer da);
 static gboolean da_drawing(GtkWidget *da, cairo_t *cr, gpointer data);
 
 int main(int argc, char **argv)
@@ -34,11 +36,18 @@ int main(int argc, char **argv)
    
    gtk_container_add(GTK_CONTAINER(window), grid);
 
+   g_timeout_add_seconds(5, (GSourceFunc)time_redraw, da);
+
    gtk_widget_show_all(window);
 
    gtk_main();
 
    return 0;  
+ }
+static gboolean time_redraw(gpointer da)
+ {
+   gtk_widget_queue_draw(GTK_WIDGET(da));
+   return TRUE;
  }
 static gboolean da_drawing(GtkWidget *da, cairo_t *cr, gpointer data)
  {
@@ -138,6 +147,68 @@ static gboolean da_drawing(GtkWidget *da, cairo_t *cr, gpointer data)
    cairo_pattern_destroy(pattern3);
    cairo_pattern_destroy(pattern4);
 
+   //Set the clock text.
+   gint i=0;
+   cairo_set_source_rgb(cr, 1.0, 1.0, 1.0);
+   gchar *hours[]={"12", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"};
+   gdouble hour_start=-G_PI/2.0;
+   gdouble next_hour=-G_PI/6.0;
+   //Start at 12 or radius with just the y component.
+   gdouble hour_radius=3.5*h1;
+   gdouble temp_cos=0;
+   gdouble temp_sin=0;
+   cairo_select_font_face(cr, "Arial", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_BOLD);
+   cairo_text_extents_t tick_extents;
+   cairo_set_font_size(cr, 20);
+   cairo_move_to(cr, 0.0, 0.0);
+   cairo_translate(cr, width/2.0, height/2.0);
+   for(i=0;i<12;i++)
+     {
+       temp_cos=cos(hour_start-(next_hour*i));
+       temp_sin=sin(hour_start-(next_hour*i));
+       //The polar form of the equation for an ellipse to get the radius.
+       hour_radius=((3.5*w1)*(3.5*h1))/sqrt(((3.5*w1)*(3.5*w1)*temp_sin*temp_sin) + ((3.5*h1)*(3.5*h1)*temp_cos*temp_cos));
+       cairo_text_extents(cr, hours[i], &tick_extents);
+       temp_cos=temp_cos*hour_radius-tick_extents.width/2.0;
+       temp_sin=temp_sin*hour_radius+tick_extents.height/2.0;
+       cairo_move_to(cr, temp_cos, temp_sin);
+       cairo_show_text(cr, hours[i]);
+     }
+
+   //Get the current time.
+   GTimeZone *time_zone=g_time_zone_new_local();
+   GDateTime *date_time=g_date_time_new_now(time_zone);
+   gdouble hour=(gdouble)g_date_time_get_hour(date_time);
+   gdouble minute=(gdouble)g_date_time_get_minute(date_time);
+   hour=hour+minute/60.0;
+   g_time_zone_unref(time_zone);
+   g_date_time_unref(date_time);
+   if(hour>12) hour=hour-12;
+
+   //Hour hand.
+   cairo_set_source_rgb(cr, 0.0, 0.0, 1.0);
+   cairo_set_line_width(cr, 6.0);
+   cairo_set_line_cap(cr, CAIRO_LINE_CAP_ROUND);
+   cairo_move_to(cr, 0.0, 0.0);
+   temp_cos=cos(hour_start-(next_hour*hour));
+   temp_sin=sin(hour_start-(next_hour*hour));
+   hour_radius=((2.0*w1)*(2.0*h1))/sqrt(((2.0*w1)*(2.0*w1)*temp_sin*temp_sin) + ((2.0*h1)*(2.0*h1)*temp_cos*temp_cos));
+   temp_cos=temp_cos*hour_radius;
+   temp_sin=temp_sin*hour_radius;
+   cairo_line_to(cr, temp_cos, temp_sin);
+   cairo_stroke(cr);
+
+   //Minute hand.
+   gdouble next_minute=-G_PI/(6.0*5.0);
+   cairo_move_to(cr, 0.0, 0.0);
+   temp_cos=cos(hour_start-(next_minute*minute));
+   temp_sin=sin(hour_start-(next_minute*minute));
+   hour_radius=((3.0*w1)*(3.0*h1))/sqrt(((3.0*w1)*(3.0*w1)*temp_sin*temp_sin) + ((3.0*h1)*(3.0*h1)*temp_cos*temp_cos));
+   temp_cos=temp_cos*hour_radius;
+   temp_sin=temp_sin*hour_radius;
+   cairo_line_to(cr, temp_cos, temp_sin);
+   cairo_stroke(cr);
+  
    return FALSE;
 }
 
