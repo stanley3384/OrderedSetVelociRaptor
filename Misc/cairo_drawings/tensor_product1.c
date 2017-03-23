028265a6efdcc50d92c9dcf38706c9b26f9e62d5
eric <cecashon@aol.com>
2017-03-23 12:12:08 -0700

    Add a fish to tensor_product1.c

--- Misc/cairo_drawings/tensor_product1.c
+++ Misc/cairo_drawings/tensor_product1.c
@@ -1,7 +1,7 @@
 
 /*
     Draw a simple tensor-product patch mesh to get an idea how it works. Then "tile" the 
-layout with the patches.
+layout and clip the mesh region in a circle and a fish.
    
     gcc -Wall tensor_product1.c -o tensor_product1 `pkg-config --cflags --libs gtk+-3.0`
 
@@ -12,11 +12,10 @@ layout with the patches.
 
 #include<gtk/gtk.h>
 
-static gboolean tile=FALSE;
-static gboolean clip_tiles=FALSE;
-
 static void combo_changed(GtkComboBox *combo_box, gpointer data);
 static gboolean da_drawing(GtkWidget *da, cairo_t *cr, gpointer data);
+static void mesh_drawing(cairo_t *cr, gint width, gint height, gint drawing_id);
+static void mesh_drawing_fish(cairo_t *cr, gint width, gint height);
 static gboolean draw_background(GtkWidget *widget, cairo_t *cr, gpointer data);
 
 int main(int argc, char **argv)
@@ -29,28 +28,29 @@ int main(int argc, char **argv)
    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
    gtk_widget_set_app_paintable(window, TRUE);
-    //Try to set transparency of main window.
-    if(gtk_widget_is_composited(window))
-      {
-        GdkScreen *screen=gtk_widget_get_screen(window);  
-        GdkVisual *visual=gdk_screen_get_rgba_visual(screen);
-        gtk_widget_set_visual(window, visual);
-      }
-    else g_print("Can't set window transparency.\n");
-    g_signal_connect(window, "draw", G_CALLBACK(draw_background), NULL);
+   //Try to set transparency of main window.
+   if(gtk_widget_is_composited(window))
+     {
+       GdkScreen *screen=gtk_widget_get_screen(window);  
+       GdkVisual *visual=gdk_screen_get_rgba_visual(screen);
+       gtk_widget_set_visual(window, visual);
+     }
+   else g_print("Can't set window transparency.\n");
+   g_signal_connect(window, "draw", G_CALLBACK(draw_background), NULL);
 
    GtkWidget *da=gtk_drawing_area_new();
    gtk_widget_set_hexpand(da, TRUE);
    gtk_widget_set_vexpand(da, TRUE);
-   g_signal_connect(da, "draw", G_CALLBACK(da_drawing), NULL);
 
    GtkWidget *combo=gtk_combo_box_text_new();
    gtk_combo_box_text_insert(GTK_COMBO_BOX_TEXT(combo), 0, "1", "Patch Layout");
    gtk_combo_box_text_insert(GTK_COMBO_BOX_TEXT(combo), 1, "2", "Tile Patches");
-   gtk_combo_box_text_insert(GTK_COMBO_BOX_TEXT(combo), 2, "3", "Clip Tiles");
+   gtk_combo_box_text_insert(GTK_COMBO_BOX_TEXT(combo), 2, "3", "Clip Tiles Circle");
+   gtk_combo_box_text_insert(GTK_COMBO_BOX_TEXT(combo), 3, "4", "Clip Tiles Fish");
    gtk_combo_box_set_active(GTK_COMBO_BOX(combo), 0);
    gtk_widget_set_hexpand(combo, TRUE);
    g_signal_connect(combo, "changed", G_CALLBACK(combo_changed), da);
+   g_signal_connect(da, "draw", G_CALLBACK(da_drawing), combo);
    
    GtkWidget *grid=gtk_grid_new();
    gtk_grid_attach(GTK_GRID(grid), da, 0, 0, 1, 1);
@@ -66,30 +66,20 @@ int main(int argc, char **argv)
  }
 static void combo_changed(GtkComboBox *combo_box, gpointer data)
  {
-   gint combo_id=gtk_combo_box_get_active(combo_box);
-   switch(combo_id)
-      {
-        case 0:
-          tile=FALSE;
-          clip_tiles=FALSE;
-          break;
-        case 1:
-          tile=TRUE;
-          clip_tiles=FALSE;
-          break;
-        case 2:
-          tile=TRUE;
-          clip_tiles=TRUE;
-      }
    gtk_widget_queue_draw(GTK_WIDGET(data));
  }
 static gboolean da_drawing(GtkWidget *da, cairo_t *cr, gpointer data)
  {
-   gint i=0;
-   gint j=0;
    gdouble width=(gdouble)gtk_widget_get_allocated_width(da);
    gdouble height=(gdouble)gtk_widget_get_allocated_height(da);
-
+   gint combo_id=gtk_combo_box_get_active(GTK_COMBO_BOX(data));
+   mesh_drawing(cr, width, height, combo_id);
+   return FALSE;
+ }
+static void mesh_drawing(cairo_t *cr, gint width, gint height, gint drawing_id)
+ {
+   gint i=0;
+   gint j=0;
    //Layout for the drawing is a 10x10 rectangle.
    gdouble w1=width/10.0;
    gdouble h1=height/10.0;
@@ -99,14 +89,23 @@ static gboolean da_drawing(GtkWidget *da, cairo_t *cr, gpointer data)
    cairo_paint(cr);
 
    //Clip tiles with a circle.
-   if(clip_tiles==TRUE)
+   if(drawing_id==2)
      {
        cairo_set_source_rgba(cr, 0.0, 0.0, 0.0, 0.0);
-       cairo_arc(cr, width/2.0, height/2.0, 3.0*w1, 0.0, 2.0*G_PI);
+       cairo_arc(cr, width/2.0, height/2.0, 3.0*h1, 0.0, 2.0*G_PI);
        cairo_clip(cr);
      }
 
-   if(tile==TRUE)
+   //Clip tiles with a fish.
+   if(drawing_id==3)
+     {
+       cairo_set_source_rgba(cr, 0.0, 0.0, 0.0, 0.0);
+       mesh_drawing_fish(cr, width, height);
+       cairo_clip(cr);
+     }
+
+
+   if(drawing_id==1||drawing_id==2||drawing_id==3)
      {
        cairo_scale(cr, 0.25, 0.25);
        cairo_translate(cr, 3.0*w1, 3.0*h1);
@@ -172,7 +171,7 @@ static gboolean da_drawing(GtkWidget *da, cairo_t *cr, gpointer data)
    cairo_mesh_pattern_set_corner_color_rgb(pattern4, 3, 0.0, 0.0, 1.0);
    cairo_mesh_pattern_end_patch(pattern4);
 
-   if(tile==TRUE)
+   if(drawing_id==1||drawing_id==2||drawing_id==3)
      {
        for(i=0;i<4;i++)
          {
@@ -208,7 +207,7 @@ static gboolean da_drawing(GtkWidget *da, cairo_t *cr, gpointer data)
    cairo_pattern_destroy(pattern3);
    cairo_pattern_destroy(pattern4);
 
-   if(tile==FALSE)
+   if(drawing_id==0)
      {
        cairo_set_source_rgb(cr, 1.0, 1.0, 0.0);
        cairo_set_line_width(cr, 4.0);
@@ -236,17 +235,46 @@ static gboolean da_drawing(GtkWidget *da, cairo_t *cr, gpointer data)
 
        //Layout axis for drawing.
        cairo_set_source_rgb(cr, 0.0, 0.0, 1.0);
-       cairo_rectangle(cr, width/10.0, height/10.0, 8.0*width/10.0, 8.0*height/10.0);
+       cairo_rectangle(cr, w1, h1, 8.0*w1, 8.0*h1);
        cairo_stroke(cr);
-       cairo_move_to(cr, 1.0*width/10.0, 5.0*height/10.0);
-       cairo_line_to(cr, 9.0*width/10.0, 5.0*height/10.0);
+       cairo_move_to(cr, 1.0*w1, 5.0*h1);
+       cairo_line_to(cr, 9.0*w1, 5.0*h1);
        cairo_stroke(cr);
-       cairo_move_to(cr, 5.0*width/10.0, 1.0*height/10.0);
-       cairo_line_to(cr, 5.0*width/10.0, 9.0*height/10.0);
+       cairo_move_to(cr, 5.0*w1, 1.0*h1);
+       cairo_line_to(cr, 5.0*w1, 9.0*h1);
        cairo_stroke(cr);
      }
 
-   return FALSE;
+ }
+static void mesh_drawing_fish(cairo_t *cr, gint width, gint height)
+ {
+   //Layout for the drawing is a 10x10 rectangle.
+   gdouble w1=width/10.0;
+   gdouble h1=height/10.0;
+
+   cairo_set_source_rgba(cr, 1.0, 1.0, 0.0, 1.0);
+   cairo_set_line_width(cr, 6.0);
+   cairo_set_line_join(cr, CAIRO_LINE_JOIN_ROUND);
+   cairo_set_line_cap(cr, CAIRO_LINE_CAP_ROUND);
+   cairo_move_to(cr, 1.0*w1, 3.0*h1);
+   cairo_curve_to(cr, 2.0*w1, 3.0*h1, 2.0*w1, 3.0*h1, 3.0*w1, 4.5*h1);
+   cairo_stroke_preserve(cr);  
+   cairo_curve_to(cr, 4.0*w1, 2.0*h1, 4.0*w1, 2.0*h1, 6.0*w1, 2.0*h1);
+   cairo_stroke_preserve(cr);
+   cairo_curve_to(cr, 8.0*w1, 2.0*h1, 8.0*w1, 2.0*h1, 9.0*w1, 5.0*h1);
+   cairo_stroke_preserve(cr);
+   cairo_curve_to(cr, 8.0*w1, 8.0*h1, 8.0*w1, 8.0*h1, 6.0*w1, 8.0*h1);
+   cairo_stroke_preserve(cr);
+   cairo_curve_to(cr, 4.0*w1, 8.0*h1, 4.0*w1, 8.0*h1, 3.0*w1, 5.5*h1);
+   cairo_stroke_preserve(cr);
+   cairo_curve_to(cr, 2.0*w1, 7.0*h1, 2.0*w1, 7.0*h1, 1.0*w1, 7.0*h1);
+   cairo_stroke_preserve(cr);
+   cairo_curve_to(cr, 2.0*w1, 6.0*h1, 2.0*w1, 6.0*h1, 2.0*w1, 5.0*h1);
+   cairo_stroke_preserve(cr);
+   cairo_curve_to(cr, 2.0*w1, 4.0*h1, 2.0*w1, 4.0*h1, 1.0*w1, 3.0*h1);
+   cairo_stroke_preserve(cr);
+   cairo_close_path(cr);
+  
  }
 static gboolean draw_background(GtkWidget *widget, cairo_t *cr, gpointer data)
  {
