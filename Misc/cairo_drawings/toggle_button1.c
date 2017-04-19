70c248ce5ca12c4475f1fba522b70b4229374177
eric <cecashon@aol.com>
2017-04-18 21:36:45 -0700

    Remove CSS from toggle_button1.c

--- Misc/cairo_drawings/toggle_button1.c
+++ Misc/cairo_drawings/toggle_button1.c
@@ -1,11 +1,10 @@
 
 /*
-    Test drawing on a toggle button. Draw using cairo on the first button and draw using CSS
-on the second button. CSS can do basic drawing easily. Cairo can do detailed drawing with a
-little more code.
-    Tested with GTK3.10 and Ubuntu14.04
+    Test drawing on a toggle button. 
 
     gcc -Wall toggle_button1.c -o toggle_button1 `pkg-config --cflags --libs gtk+-3.0`
+
+    Tested with GTK3.18 and Ubuntu16.04
 
     C. Eric Cashon
 */
@@ -19,20 +18,20 @@ gboolean draw_button1(GtkWidget *widget, cairo_t *cr, gpointer user_data)
 
     if(gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(widget)))
       {
-        cairo_set_source_rgb(cr, 0.0, 1.0, 0.0);
+        cairo_set_source_rgb(cr, 1.0, 0.0, 0.0);
         cairo_paint(cr);
       }
     else
       {
-        cairo_set_source_rgb(cr, 0.0, 0.0, 1.0);
+        cairo_set_source_rgb(cr, 0.1, 0.1, 0.1);
         cairo_paint(cr);
       }
     
-    //Draw purple rectangle around the button.
-    cairo_set_line_width(cr, 4.0);
-    cairo_set_source_rgb(cr, 1.0, 0.0, 1.0);
+    //Draw rectangle around the button.
+    cairo_set_line_width(cr, 10.0);
+    cairo_set_source_rgb(cr, 0.0, 0.0, 0.0);
     cairo_rectangle(cr, 0, 0, width, height);
-    cairo_stroke_preserve(cr); 
+    cairo_stroke(cr); 
 
     //Add the text.
     cairo_set_source_rgb(cr, 1.0, 1.0, 0.0);
@@ -49,43 +48,23 @@ int main(int argc, char **argv)
   {      
     gtk_init(&argc, &argv);   
 
-    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    GtkWidget *window=gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
     gtk_window_set_default_size(GTK_WINDOW(window), 200, 100);
-    gtk_window_set_title(GTK_WINDOW(window), "Toggle Buttons");
+    gtk_window_set_title(GTK_WINDOW(window), "Toggle Button");
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
-    GtkWidget *button1 = gtk_toggle_button_new_with_label("button1");
+    GtkWidget *button1=gtk_toggle_button_new_with_label("button1");
     gtk_widget_set_app_paintable(button1, TRUE);
     gtk_widget_set_hexpand(button1, TRUE);
     gtk_widget_set_vexpand(button1, TRUE);
     g_signal_connect(button1, "draw", G_CALLBACK(draw_button1), NULL);
-
-    GtkWidget *button2 = gtk_toggle_button_new_with_label("Standard Toggle");
-    gtk_widget_set_hexpand(button2, TRUE);
-    gtk_widget_set_vexpand(button2, TRUE);
       
-    GtkWidget *grid = gtk_grid_new();
+    GtkWidget *grid=gtk_grid_new();
     gtk_container_set_border_width(GTK_CONTAINER(grid), 10);
     gtk_grid_attach(GTK_GRID(grid), button1, 0, 0, 1, 1);
-    gtk_grid_attach(GTK_GRID(grid), button2, 0, 1, 1, 1);
 
     gtk_container_add(GTK_CONTAINER(window), grid);
-
-    //Draw second toggle button with CSS.
-    GError *css_error = NULL;
-    gchar css_string[] = "GtkToggleButton:active{background: #00ff00; color: #ffff00} GtkToggleButton{background: #0000ff; color: #ffff00}";
-    GtkCssProvider *provider = gtk_css_provider_new();
-    GdkDisplay *display = gdk_display_get_default();
-    GdkScreen *screen = gdk_display_get_default_screen(display);
-    gtk_style_context_add_provider_for_screen(screen, GTK_STYLE_PROVIDER(provider), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
-    gtk_css_provider_load_from_data(provider, css_string, -1, &css_error);
-    if(css_error!=NULL)
-      {
-        g_print("CSS loader error %s\n", css_error->message);
-        g_error_free(css_error);
-      }
-    g_object_unref(provider);
 
     gtk_widget_show_all(window);                  
     gtk_main();
