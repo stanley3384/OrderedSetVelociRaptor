fd5d644538d15c7501ed5afa6d13e419e8e0e246
eric <cecashon@aol.com>
2017-05-09 11:08:11 -0700

    Skip drawing some trapezoids in circular_gradient2.c

--- Misc/cairo_drawings/circular_gradient2.c
+++ Misc/cairo_drawings/circular_gradient2.c
@@ -2,7 +2,8 @@
 /*   
     Remove the Bezier curves from circular_gradient1.c and just use the trapezoids to draw the ring.
 A little easier and it should work fine if you have enough trapezoids to approximate a ring. Still
-a problem of mapping a discrete radian angle from the trapezoids to a continuous number on a arc.
+a problem of mapping a discrete radian angle from the trapezoids with gradients to a continuous
+number on a arc.
 
     gcc -Wall circular_gradient2.c -o circular_gradient2 `pkg-config --cflags --libs gtk+-3.0` -lm
 
@@ -19,17 +20,22 @@ static void draw_circle(GtkWidget *da, cairo_t *cr, gdouble next_section, gint s
 static void combo1_changed(GtkComboBox *combo1, gpointer data);
 static void combo2_changed(GtkComboBox *combo2, gpointer data);
 static void combo3_changed(GtkComboBox *combo3, gpointer data);
+static void combo4_changed(GtkComboBox *combo4, gpointer data);
 static void toggle_fade(GtkToggleButton *check1, gpointer data);
+static void time_drawing(GtkToggleButton *check2, gpointer data);
 static void check_colors(GtkWidget *widget, GtkWidget **colors);
 
-//Test a colors to start and stop the gradient.
+//Test a colors to start and stop the gradients. Set by UI entries.
 static gdouble color_start[]={0.0, 1.0, 0.0, 1.0};
 static gdouble color_mid[]={1.0, 1.0, 0.0, 1.0};
 static gdouble color_stop[]={0.0, 0.0, 1.0, 1.0};
-static gdouble cutoff1=100.0;
+//Variables set by the UI combos and check boxes.
+static gdouble mid_color_pos=100.0;
 static gint drawing_combo=0;
 static gint rotate_combo=0;
+static gint skip_combo=0;
 static gboolean fade=FALSE;
+static gboolean time_it=FALSE;
 
 int main(int argc, char **argv)
  {
@@ -61,6 +67,10 @@ int main(int argc, char **argv)
    gtk_widget_set_halign(check1, GTK_ALIGN_CENTER);
    g_signal_connect(check1, "toggled", G_CALLBACK(toggle_fade), da);
 
+   GtkWidget *check2=gtk_check_button_new_with_label("Time Drawing");
+   gtk_widget_set_halign(check2, GTK_ALIGN_CENTER);
+   g_signal_connect(check2, "toggled", G_CALLBACK(time_drawing), NULL);
+
    GtkWidget *combo2=gtk_combo_box_text_new();
    gtk_widget_set_hexpand(combo2, TRUE);
    gtk_combo_box_text_insert(GTK_COMBO_BOX_TEXT(combo2), 0, "1", "Rotate 0");
@@ -72,13 +82,21 @@ int main(int argc, char **argv)
    g_signal_connect(combo2, "changed", G_CALLBACK(combo2_changed), da);
 
    GtkWidget *combo3=gtk_combo_box_text_new();
-   gtk_widget_set_hexpand(combo2, TRUE);
+   gtk_widget_set_hexpand(combo3, TRUE);
    gtk_combo_box_text_insert(GTK_COMBO_BOX_TEXT(combo3), 0, "1", "No Mid Color");
    gtk_combo_box_text_insert(GTK_COMBO_BOX_TEXT(combo3), 1, "2", "Mid Color Stop 75%");
    gtk_combo_box_text_insert(GTK_COMBO_BOX_TEXT(combo3), 2, "3", "Mid Color Stop 50%");
    gtk_combo_box_text_insert(GTK_COMBO_BOX_TEXT(combo3), 3, "4", "Mid Color Stop 25%");
    gtk_combo_box_set_active(GTK_COMBO_BOX(combo3), 0);
    g_signal_connect(combo3, "changed", G_CALLBACK(combo3_changed), da);
+
+   GtkWidget *combo4=gtk_combo_box_text_new();
+   gtk_widget_set_hexpand(combo4, TRUE);
+   gtk_combo_box_text_insert(GTK_COMBO_BOX_TEXT(combo4), 0, "1", "Skip 0");
+   gtk_combo_box_text_insert(GTK_COMBO_BOX_TEXT(combo4), 1, "2", "Skip 2");
+   gtk_combo_box_text_insert(GTK_COMBO_BOX_TEXT(combo4), 2, "3", "Skip 4");
+   gtk_combo_box_set_active(GTK_COMBO_BOX(combo4), 0);
+   g_signal_connect(combo4, "changed", G_CALLBACK(combo4_changed), da);
 
    GtkWidget *label1=gtk_label_new("Start Color");
    gtk_widget_set_hexpand(label1, TRUE);
@@ -114,13 +132,15 @@ int main(int argc, char **argv)
    gtk_grid_attach(GTK_GRID(grid), check1, 0, 1, 2, 1);
    gtk_grid_attach(GTK_GRID(grid), combo2, 0, 2, 2, 1);
    gtk_grid_attach(GTK_GRID(grid), combo3, 0, 3, 2, 1);
-   gtk_grid_attach(GTK_GRID(grid), label1, 0, 4, 1, 1);
-   gtk_grid_attach(GTK_GRID(grid), entry1, 1, 4, 1, 1);
-   gtk_grid_attach(GTK_GRID(grid), label2, 0, 5, 1, 1);
-   gtk_grid_attach(GTK_GRID(grid), entry2, 1, 5, 1, 1);
-   gtk_grid_attach(GTK_GRID(grid), label3, 0, 6, 1, 1);
-   gtk_grid_attach(GTK_GRID(grid), entry3, 1, 6, 1, 1);
-   gtk_grid_attach(GTK_GRID(grid), button1, 0, 7, 2, 1);
+   gtk_grid_attach(GTK_GRID(grid), combo4, 0, 4, 2, 1);
+   gtk_grid_attach(GTK_GRID(grid), check2, 0, 5, 2, 1);
+   gtk_grid_attach(GTK_GRID(grid), label1, 0, 6, 1, 1);
+   gtk_grid_attach(GTK_GRID(grid), entry1, 1, 6, 1, 1);
+   gtk_grid_attach(GTK_GRID(grid), label2, 0, 7, 1, 1);
+   gtk_grid_attach(GTK_GRID(grid), entry2, 1, 7, 1, 1);
+   gtk_grid_attach(GTK_GRID(grid), label3, 0, 8, 1, 1);
+   gtk_grid_attach(GTK_GRID(grid), entry3, 1, 8, 1, 1);   
+   gtk_grid_attach(GTK_GRID(grid), button1, 0, 9, 2, 1);
 
    GtkWidget *paned1=gtk_paned_new(GTK_ORIENTATION_HORIZONTAL);
    gtk_paned_pack1(GTK_PANED(paned1), grid, FALSE, TRUE);
@@ -141,6 +161,9 @@ static gboolean da_drawing(GtkWidget *da, cairo_t *cr, gpointer data)
    gdouble height=(gdouble)gtk_widget_get_allocated_height(da);
    gdouble r1=0;
    gdouble w1=0;
+   GTimer *timer=NULL;
+ 
+   if(time_it==TRUE) timer=g_timer_new();
 
    //Scale.
    if(width<height) w1=width/10.0;
@@ -189,6 +212,13 @@ static gboolean da_drawing(GtkWidget *da, cairo_t *cr, gpointer data)
        cairo_arc(cr, width/2.0, height/2.0, 2.7*w1, 0.0, 2.0*G_PI);
        cairo_fill(cr); 
      }
+
+   if(time_it==TRUE) 
+     {
+       g_print("Draw Time %f\n", g_timer_elapsed(timer, NULL));
+       g_timer_destroy(timer);
+     }
+
    return FALSE;
  }
 static void draw_circle(GtkWidget *da, cairo_t *cr, gdouble next_section, gint sections, gdouble r1)
@@ -198,7 +228,7 @@ static void draw_circle(GtkWidget *da, cairo_t *cr, gdouble next_section, gint s
    gdouble height=(gdouble)gtk_widget_get_allocated_height(da);
    gdouble w1=0;
 
-   //Scale.
+   //Scale. Keep square.
    if(width<height) w1=width/10.0;
    else w1=height/10.0;
 
@@ -206,6 +236,7 @@ static void draw_circle(GtkWidget *da, cairo_t *cr, gdouble next_section, gint s
    cairo_translate(cr, width/2.0, height/2.0);
    cairo_rotate(cr, r1); 
    
+   //Draw the trapezoids and paint them with a gradient.
    gdouble start=0.0;
    gdouble line_radius1=0;
    gdouble line_radius2=0;
@@ -217,6 +248,11 @@ static void draw_circle(GtkWidget *da, cairo_t *cr, gdouble next_section, gint s
    gdouble prev_sin1=0.0;
    gdouble prev_cos2=4.0*w1;
    gdouble prev_sin2=0.0;
+   //Colors for gradients.
+   gdouble color_start1[3];
+   gdouble color_mid1[3];
+   gdouble color_stop1[3];
+   gint stop=(gint)(mid_color_pos*(gdouble)sections/100.0);
    for(i=0;i<sections;i++)
      {
        temp_cos1=cos(start-(next_section*(i+1)));
@@ -233,9 +269,6 @@ static void draw_circle(GtkWidget *da, cairo_t *cr, gdouble next_section, gint s
        temp_sin2=temp_sin2*line_radius2;
         
        //Set up colors for the gradients.
-       gdouble color_start1[3];
-       gdouble color_mid1[3];
-       gdouble color_stop1[3];
        color_start1[0]=color_start[0];
        color_start1[1]=color_start[1];
        color_start1[2]=color_start[2];
@@ -245,10 +278,9 @@ static void draw_circle(GtkWidget *da, cairo_t *cr, gdouble next_section, gint s
        color_stop1[0]=color_stop[0];
        color_stop1[1]=color_stop[1];
        color_stop1[2]=color_stop[2];
-       gint stop=(gint)(cutoff1*(gdouble)sections/100.0);
        if(fade)
          {   
-           if(cutoff1==100)
+           if(mid_color_pos==100)
              {
                gdouble diff0=color_stop[0]-color_start[0];
                gdouble diff1=color_stop[1]-color_start[1];
@@ -286,39 +318,45 @@ static void draw_circle(GtkWidget *da, cairo_t *cr, gdouble next_section, gint s
              }
          }
 
-       //Draw the gradients.        
-       cairo_pattern_t *pattern1=cairo_pattern_create_mesh();
-       cairo_mesh_pattern_begin_patch(pattern1);
-       cairo_mesh_pattern_move_to(pattern1, prev_cos2, prev_sin2);
-       cairo_mesh_pattern_line_to(pattern1, temp_cos2, temp_sin2);
-       cairo_mesh_pattern_line_to(pattern1, temp_cos1, temp_sin1);
-       cairo_mesh_pattern_line_to(pattern1, prev_cos1, prev_sin1);
-       cairo_mesh_pattern_line_to(pattern1, prev_cos2, prev_sin2);
-       if(cutoff1==100)
-         {
-           cairo_mesh_pattern_set_corner_color_rgba(pattern1, 0, color_start1[0], color_start1[1], color_start1[2], color_start[3]);
-           cairo_mesh_pattern_set_corner_color_rgba(pattern1, 1, color_stop1[0], color_stop1[1], color_stop1[2], color_stop[3]);
-           cairo_mesh_pattern_set_corner_color_rgba(pattern1, 2, color_stop1[0], color_stop1[1], color_stop1[2], color_stop[3]);
-           cairo_mesh_pattern_set_corner_color_rgba(pattern1, 3, color_start1[0], color_start1[1], color_start1[2], color_start[3]);
-         }
-       else if(i<stop)
-         {
-           cairo_mesh_pattern_set_corner_color_rgba(pattern1, 0, color_start1[0], color_start1[1], color_start1[2], color_start[3]);
-           cairo_mesh_pattern_set_corner_color_rgba(pattern1, 1, color_mid1[0], color_mid1[1], color_mid1[2], color_mid[3]);
-           cairo_mesh_pattern_set_corner_color_rgba(pattern1, 2, color_mid1[0], color_mid1[1], color_mid1[2], color_mid[3]);
-           cairo_mesh_pattern_set_corner_color_rgba(pattern1, 3, color_start1[0], color_start1[1], color_start1[2], color_start[3]);
-         }
-       else
-         {
-           cairo_mesh_pattern_set_corner_color_rgba(pattern1, 0, color_mid1[0], color_mid1[1], color_mid1[2], color_mid[3]);
-           cairo_mesh_pattern_set_corner_color_rgba(pattern1, 1, color_stop1[0], color_stop1[1], color_stop1[2], color_stop[3]);
-           cairo_mesh_pattern_set_corner_color_rgba(pattern1, 2, color_stop1[0], color_stop1[1], color_stop1[2], color_stop[3]);
-           cairo_mesh_pattern_set_corner_color_rgba(pattern1, 3, color_mid1[0], color_mid1[1], color_mid1[2], color_mid[3]);
-         }
-       cairo_mesh_pattern_end_patch(pattern1);
-       cairo_set_source(cr, pattern1);
-       cairo_paint(cr);
-       cairo_pattern_destroy(pattern1);          
+       /*
+         Draw the gradients. Can skip the drawing of a trapezoid for example a gauge might
+         not want a full circle.
+       */
+       if(i>=skip_combo&&i<=sections-skip_combo-1)
+         {       
+           cairo_pattern_t *pattern1=cairo_pattern_create_mesh();
+           cairo_mesh_pattern_begin_patch(pattern1);
+           cairo_mesh_pattern_move_to(pattern1, prev_cos2, prev_sin2);
+           cairo_mesh_pattern_line_to(pattern1, temp_cos2, temp_sin2);
+           cairo_mesh_pattern_line_to(pattern1, temp_cos1, temp_sin1);
+           cairo_mesh_pattern_line_to(pattern1, prev_cos1, prev_sin1);
+           cairo_mesh_pattern_line_to(pattern1, prev_cos2, prev_sin2);
+           if(mid_color_pos==100)
+             {
+               cairo_mesh_pattern_set_corner_color_rgba(pattern1, 0, color_start1[0], color_start1[1], color_start1[2], color_start[3]);
+               cairo_mesh_pattern_set_corner_color_rgba(pattern1, 1, color_stop1[0], color_stop1[1], color_stop1[2], color_stop[3]);
+               cairo_mesh_pattern_set_corner_color_rgba(pattern1, 2, color_stop1[0], color_stop1[1], color_stop1[2], color_stop[3]);
+               cairo_mesh_pattern_set_corner_color_rgba(pattern1, 3, color_start1[0], color_start1[1], color_start1[2], color_start[3]);
+             }
+           else if(i<stop)
+             {
+               cairo_mesh_pattern_set_corner_color_rgba(pattern1, 0, color_start1[0], color_start1[1], color_start1[2], color_start[3]);
+               cairo_mesh_pattern_set_corner_color_rgba(pattern1, 1, color_mid1[0], color_mid1[1], color_mid1[2], color_mid[3]);
+               cairo_mesh_pattern_set_corner_color_rgba(pattern1, 2, color_mid1[0], color_mid1[1], color_mid1[2], color_mid[3]);
+               cairo_mesh_pattern_set_corner_color_rgba(pattern1, 3, color_start1[0], color_start1[1], color_start1[2], color_start[3]);
+             }
+           else
+             {
+               cairo_mesh_pattern_set_corner_color_rgba(pattern1, 0, color_mid1[0], color_mid1[1], color_mid1[2], color_mid[3]);
+               cairo_mesh_pattern_set_corner_color_rgba(pattern1, 1, color_stop1[0], color_stop1[1], color_stop1[2], color_stop[3]);
+               cairo_mesh_pattern_set_corner_color_rgba(pattern1, 2, color_stop1[0], color_stop1[1], color_stop1[2], color_stop[3]);
+               cairo_mesh_pattern_set_corner_color_rgba(pattern1, 3, color_mid1[0], color_mid1[1], color_mid1[2], color_mid[3]);
+             }
+           cairo_mesh_pattern_end_patch(pattern1);
+           cairo_set_source(cr, pattern1);
+           cairo_paint(cr);
+           cairo_pattern_destroy(pattern1);
+         }          
 
        //Polygon
        if(drawing_combo<3)
@@ -355,10 +393,15 @@ static void combo2_changed(GtkComboBox *combo2, gpointer data)
 static void combo3_changed(GtkComboBox *combo3, gpointer data)
  {
    gint row=gtk_combo_box_get_active(combo3);
-   if(row==0) cutoff1=100.0;
-   else if(row==1) cutoff1=75.0;
-   else if(row==2) cutoff1=50.0;
-   else cutoff1=25.0;
+   if(row==0) mid_color_pos=100.0;
+   else if(row==1) mid_color_pos=75.0;
+   else if(row==2) mid_color_pos=50.0;
+   else mid_color_pos=25.0;
+   gtk_widget_queue_draw(GTK_WIDGET(data));
+ }
+static void combo4_changed(GtkComboBox *combo4, gpointer data)
+ {
+   skip_combo=gtk_combo_box_get_active(combo4);
    gtk_widget_queue_draw(GTK_WIDGET(data));
  }
 static void toggle_fade(GtkToggleButton *check1, gpointer data)
@@ -366,6 +409,11 @@ static void toggle_fade(GtkToggleButton *check1, gpointer data)
    if(gtk_toggle_button_get_active(check1)) fade=TRUE;
    else fade=FALSE;
    gtk_widget_queue_draw(GTK_WIDGET(data));
+ }
+static void time_drawing(GtkToggleButton *check2, gpointer data)
+ {
+   if(gtk_toggle_button_get_active(check2)) time_it=TRUE;
+   else time_it=FALSE;
  }
 static void check_colors(GtkWidget *widget, GtkWidget **colors)
   {
