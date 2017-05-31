98daadcabbc7d5fd57e6c393f238bd6a4eced349
eric <cecashon@aol.com>
2017-05-31 15:41:36 -0700

    Add a sun to charger_icon1.c

--- Misc/cairo_drawings/charger_icon1.c
+++ Misc/cairo_drawings/charger_icon1.c
@@ -1,8 +1,8 @@
 
 /*
-    Draw a charger icon with cairo. Try a battery and speeding electron look.
+    Draw a charger icon with cairo. Try a battery, speeding electron and sun look.
 
-    gcc -Wall charger_icon1.c -o charger_icon1 `pkg-config gtk+-3.0 --cflags --libs`
+    gcc -Wall charger_icon1.c -o charger_icon1 `pkg-config gtk+-3.0 --cflags --libs` -lm
 
     Tested on Ubuntu16.04, GTK3.18.
 
@@ -10,6 +10,7 @@
 */
 
 #include<gtk/gtk.h>
+#include<math.h>
 
 static gint drawing_id=0;
 
@@ -18,6 +19,7 @@ static gboolean da_drawing(GtkWidget *da, cairo_t *cr, gpointer data);
 static void draw_charger(GtkWidget *da, cairo_t *cr);
 static void draw_gradient1(cairo_t *cr, gdouble w1, gdouble h1);
 static void draw_gradient2(cairo_t *cr, gdouble w1, gdouble h1, gdouble scale_line);
+static void draw_gradient3(cairo_t *cr, gdouble w1, gdouble h1, gdouble scale_line);
 
 int main(int argc, char *argv[])
   {
@@ -35,8 +37,9 @@ int main(int argc, char *argv[])
     g_signal_connect(da, "draw", G_CALLBACK(da_drawing), NULL);
 
     GtkWidget *combo=gtk_combo_box_text_new();
-    gtk_combo_box_text_insert(GTK_COMBO_BOX_TEXT(combo), 0, "1", "Draw Charger Icon1");
-    gtk_combo_box_text_insert(GTK_COMBO_BOX_TEXT(combo), 1, "2", "Draw Charger Icon2");
+    gtk_combo_box_text_insert(GTK_COMBO_BOX_TEXT(combo), 0, "1", "Battery");
+    gtk_combo_box_text_insert(GTK_COMBO_BOX_TEXT(combo), 1, "2", "Speeding Electron");
+    gtk_combo_box_text_insert(GTK_COMBO_BOX_TEXT(combo), 2, "3", "Sun");
     gtk_combo_box_set_active(GTK_COMBO_BOX(combo), 0);
     gtk_widget_set_hexpand(combo, TRUE);
     g_signal_connect(combo, "changed", G_CALLBACK(combo_changed), da);  
@@ -57,7 +60,8 @@ static void combo_changed(GtkComboBox *combo_box, gpointer data)
     gint combo_id=gtk_combo_box_get_active(combo_box);
 
     if(combo_id==0) drawing_id=0;
-    else drawing_id=1;
+    else if(combo_id==1) drawing_id=1;
+    else drawing_id=2;
 
     gtk_widget_queue_draw(GTK_WIDGET(data));
   }
@@ -134,7 +138,8 @@ static void draw_charger(GtkWidget *da, cairo_t *cr)
     cairo_fill(cr);
 
     if(drawing_id==0) draw_gradient1(cr, w1, h1);
-    else draw_gradient2(cr, w1, h1, scale_line);
+    else if(drawing_id==1) draw_gradient2(cr, w1, h1, scale_line);
+    else draw_gradient3(cr, w1, h1, scale_line);
 
     //Draw the top rectangle outline.
     cairo_set_source_rgb(cr, 0.0, 1.0, 0.0);
@@ -264,7 +269,7 @@ static void draw_gradient2(cairo_t *cr, gdouble w1, gdouble h1, gdouble scale_li
     cairo_save(cr);
     cairo_set_source_rgb(cr, 0.6, 0.6, 1.0);
     cairo_scale(cr, 1.0, 0.5);
-    cairo_arc(cr, 6.2*w1, 5.0*h1, 15*scale_line, 0, 2*G_PI);
+    cairo_arc(cr, 6.2*w1, 5.0*h1, 15.0*scale_line, 0, 2*G_PI);
     cairo_fill(cr);
     cairo_restore(cr);
 
@@ -275,6 +280,56 @@ static void draw_gradient2(cairo_t *cr, gdouble w1, gdouble h1, gdouble scale_li
     cairo_line_to(cr, 6.4*w1, 2.5*h1);
     cairo_stroke(cr);
     
+    cairo_pattern_destroy(pattern1);
+  }
+static void draw_gradient3(cairo_t *cr, gdouble w1, gdouble h1, gdouble scale_line)
+  {
+    gint i=0;
+
+    //Scale the sun.
+    if(w1>h1)
+      {
+        scale_line=h1*10.0/400.0;
+      }
+
+    cairo_pattern_t *pattern1=cairo_pattern_create_radial(5.0*w1, 2.5*h1, 1.0*scale_line, 5.0*w1, 2.5*h1, 50.0*scale_line);
+    cairo_pattern_add_color_stop_rgb(pattern1, 0.0, 1.0, 0.5, 0.0); 
+    cairo_pattern_add_color_stop_rgb(pattern1, 1.0, 1.0, 1.0, 0.0);
+
+    cairo_set_source(cr, pattern1);
+ 
+    cairo_save(cr);
+    //Outside points for sun rays.
+    gdouble hour_start=-G_PI/2.0;
+    gdouble next_hour=-G_PI/6.0;
+    gdouble hour_radius=50.0*scale_line;
+    gdouble temp_cos=0;
+    gdouble temp_sin=0;
+    //Inside points for the sun rays.
+    gdouble hour_start2=-G_PI/2.0+G_PI/12.0;
+    gdouble next_hour2=-G_PI/6.0;
+    gdouble hour_radius2=30.0*scale_line;
+    gdouble temp_cos2=0;
+    gdouble temp_sin2=0;
+    cairo_translate(cr, 5.0*w1, 2.5*h1); 
+    for(i=0;i<13;i++)
+      {
+        temp_cos=cos(hour_start-(next_hour*i))*hour_radius;
+        temp_sin=sin(hour_start-(next_hour*i))*hour_radius;        
+        if(i>0)
+          {
+            cairo_line_to(cr, temp_cos, temp_sin);
+            cairo_stroke_preserve(cr);  
+          } 
+        temp_cos2=cos(hour_start2-(next_hour2*i))*hour_radius2;
+        temp_sin2=sin(hour_start2-(next_hour2*i))*hour_radius2;
+        cairo_line_to(cr, temp_cos2, temp_sin2);
+        cairo_stroke_preserve(cr);                          
+     }
+    cairo_close_path(cr);
+    cairo_fill(cr);
+    cairo_restore(cr);
+
     cairo_pattern_destroy(pattern1);
   }
 
