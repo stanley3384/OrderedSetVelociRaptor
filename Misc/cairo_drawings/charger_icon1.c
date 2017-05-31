d76783c9b578ecef71b532f7ff7c5ff2cc2a7ac2
eric <cecashon@aol.com>
2017-05-31 11:42:15 -0700

    Try speeding electron look in charger_icon1.c

--- Misc/cairo_drawings/charger_icon1.c
+++ Misc/cairo_drawings/charger_icon1.c
@@ -1,6 +1,6 @@
 
 /*
-    Draw a charger icon with cairo.
+    Draw a charger icon with cairo. Try a battery and speeding electron look.
 
     gcc -Wall charger_icon1.c -o charger_icon1 `pkg-config gtk+-3.0 --cflags --libs`
 
@@ -16,6 +16,8 @@ static gint drawing_id=0;
 static void combo_changed(GtkComboBox *combo_box, gpointer data);
 static gboolean da_drawing(GtkWidget *da, cairo_t *cr, gpointer data);
 static void draw_charger(GtkWidget *da, cairo_t *cr);
+static void draw_gradient1(cairo_t *cr, gdouble w1, gdouble h1);
+static void draw_gradient2(cairo_t *cr, gdouble w1, gdouble h1, gdouble scale_line);
 
 int main(int argc, char *argv[])
   {
@@ -33,7 +35,8 @@ int main(int argc, char *argv[])
     g_signal_connect(da, "draw", G_CALLBACK(da_drawing), NULL);
 
     GtkWidget *combo=gtk_combo_box_text_new();
-    gtk_combo_box_text_insert(GTK_COMBO_BOX_TEXT(combo), 0, "1", "Draw Charger Icon");
+    gtk_combo_box_text_insert(GTK_COMBO_BOX_TEXT(combo), 0, "1", "Draw Charger Icon1");
+    gtk_combo_box_text_insert(GTK_COMBO_BOX_TEXT(combo), 1, "2", "Draw Charger Icon2");
     gtk_combo_box_set_active(GTK_COMBO_BOX(combo), 0);
     gtk_widget_set_hexpand(combo, TRUE);
     g_signal_connect(combo, "changed", G_CALLBACK(combo_changed), da);  
@@ -60,8 +63,7 @@ static void combo_changed(GtkComboBox *combo_box, gpointer data)
   }
 static gboolean da_drawing(GtkWidget *da, cairo_t *cr, gpointer data)
   {
-    if(drawing_id==0) draw_charger(da, cr);
- 
+    draw_charger(da, cr); 
     return FALSE;
   }
 static void draw_charger(GtkWidget *da, cairo_t *cr)
@@ -116,7 +118,7 @@ static void draw_charger(GtkWidget *da, cairo_t *cr)
     cairo_fill(cr);
 
     //Top rectangle background.
-    cairo_set_source_rgb(cr, 0.0, 0.0, 1.0);
+    cairo_set_source_rgb(cr, 0.0, 0.0, 0.4);
     cairo_set_line_width(cr, 1.0);
     cairo_move_to(cr, 3.0*w1, 4.0*h1);
     cairo_line_to(cr, 3.0*w1, 1.5*h1);
@@ -131,33 +133,8 @@ static void draw_charger(GtkWidget *da, cairo_t *cr)
     cairo_close_path(cr);
     cairo_fill(cr);
 
-    //Gradient in the top rectangle.
-    cairo_pattern_t *pattern1=cairo_pattern_create_linear(3.0*w1, 1.0*h1, 3.0*w1, 4.0*h1); 
-    cairo_pattern_add_color_stop_rgba(pattern1, 0.0, 0.0, 0.0, 1.0, 1.0); 
-    cairo_pattern_add_color_stop_rgba(pattern1, 0.1, 1.0, 1.0, 0.0, 1.0);
-    cairo_pattern_add_color_stop_rgba(pattern1, 0.2, 0.0, 0.0, 1.0, 1.0);
-    cairo_pattern_add_color_stop_rgba(pattern1, 0.3, 1.0, 1.0, 0.0, 1.0);
-    cairo_pattern_add_color_stop_rgba(pattern1, 0.4, 0.0, 0.0, 1.0, 1.0);
-    cairo_pattern_add_color_stop_rgba(pattern1, 0.5, 1.0, 1.0, 0.0, 1.0); 
-    cairo_pattern_add_color_stop_rgba(pattern1, 0.6, 0.0, 0.0, 1.0, 1.0);
-    cairo_pattern_add_color_stop_rgba(pattern1, 0.7, 1.0, 1.0, 0.0, 1.0);
-    cairo_pattern_add_color_stop_rgba(pattern1, 0.8, 0.0, 0.0, 1.0, 1.0);
-    cairo_pattern_add_color_stop_rgba(pattern1, 0.9, 1.0, 1.0, 0.0, 1.0);
-    cairo_pattern_add_color_stop_rgba(pattern1, 1.0, 0.0, 0.0, 1.0, 1.0);      
-    cairo_set_source(cr, pattern1);    
-    cairo_rectangle(cr, 3.3*w1, 1.0*h1, 0.5*w1, 3.0*h1);
-    cairo_fill(cr);
-    cairo_rectangle(cr, 3.9*w1, 1.0*h1, 0.5*w1, 3.0*h1);
-    cairo_fill(cr);
-    cairo_rectangle(cr, 4.5*w1, 1.0*h1, 0.5*w1, 3.0*h1);
-    cairo_fill(cr);
-    cairo_rectangle(cr, 5.1*w1, 1.0*h1, 0.5*w1, 3.0*h1);
-    cairo_fill(cr);
-    cairo_rectangle(cr, 5.7*w1, 1.0*h1, 0.5*w1, 3.0*h1);
-    cairo_fill(cr);
-    cairo_rectangle(cr, 6.3*w1, 1.0*h1, 0.5*w1, 3.0*h1);
-    cairo_fill(cr);
-    cairo_pattern_destroy(pattern1);
+    if(drawing_id==0) draw_gradient1(cr, w1, h1);
+    else draw_gradient2(cr, w1, h1, scale_line);
 
     //Draw the top rectangle outline.
     cairo_set_source_rgb(cr, 0.0, 1.0, 0.0);
@@ -189,7 +166,7 @@ static void draw_charger(GtkWidget *da, cairo_t *cr)
     cairo_stroke(cr);
 
     //Rectangle on the side of the battery.
-    cairo_set_source_rgb(cr, 0.0, 0.0, 1.0);
+    cairo_set_source_rgb(cr, 0.0, 0.0, 0.4);
     cairo_set_line_width(cr, 1.0);
     cairo_rectangle(cr, 6.75*w1, 2.25*h1, 0.25*w1, 0.5*h1);
     cairo_fill(cr);
@@ -234,4 +211,71 @@ static void draw_charger(GtkWidget *da, cairo_t *cr)
     cairo_fill(cr);
     cairo_stroke(cr);   
   }
+static void draw_gradient1(cairo_t *cr, gdouble w1, gdouble h1)
+  {
+    //Gradient in the top rectangle.
+    cairo_pattern_t *pattern1=cairo_pattern_create_linear(3.0*w1, 1.0*h1, 3.0*w1, 4.0*h1);
+    cairo_pattern_add_color_stop_rgba(pattern1, 0.0, 0.0, 0.0, 0.4, 1.0); 
+    cairo_pattern_add_color_stop_rgba(pattern1, 0.5, 1.0, 1.0, 0.0, 1.0);
+    cairo_pattern_add_color_stop_rgba(pattern1, 1.0, 0.0, 0.0, 0.4, 1.0);
+         
+    cairo_set_source(cr, pattern1);    
+    cairo_rectangle(cr, 3.3*w1, 1.0*h1, 0.5*w1, 3.0*h1);
+    cairo_fill(cr);
+    cairo_rectangle(cr, 3.9*w1, 1.0*h1, 0.5*w1, 3.0*h1);
+    cairo_fill(cr);
+    cairo_rectangle(cr, 4.5*w1, 1.0*h1, 0.5*w1, 3.0*h1);
+    cairo_fill(cr);
+    cairo_rectangle(cr, 5.1*w1, 1.0*h1, 0.5*w1, 3.0*h1);
+    cairo_fill(cr);
+    cairo_rectangle(cr, 5.7*w1, 1.0*h1, 0.5*w1, 3.0*h1);
+    cairo_fill(cr);
+    cairo_rectangle(cr, 6.3*w1, 1.0*h1, 0.5*w1, 3.0*h1);
+    cairo_fill(cr);
+    cairo_pattern_destroy(pattern1);
+  }
+static void draw_gradient2(cairo_t *cr, gdouble w1, gdouble h1, gdouble scale_line)
+  {
+    gint i=0;
+
+    //Gradient in the top rectangle.
+    cairo_pattern_t *pattern1=cairo_pattern_create_linear(3.0*w1, 1.0*h1, 3.0*w1, 4.0*h1);
+    for(i=0;i<=20;i+=2)
+      { 
+        cairo_pattern_add_color_stop_rgb(pattern1, (double)(i/20.0), 1.0, 1.0, 0.0); 
+        cairo_pattern_add_color_stop_rgb(pattern1, (double)(i+1.0)/20.0, 1.0, 0.0, 0.0); 
+      }
+ 
+    //The wave.    
+    cairo_save(cr);
+    cairo_set_source(cr, pattern1);
+    cairo_set_line_width(cr, 3);
+    cairo_move_to(cr, 3.0*w1, 1.2*h1);
+    cairo_curve_to(cr, 4.0*w1, 2.2*h1, 5.0*w1, 2.4*h1, 5.5*w1, 2.5*h1);
+    cairo_stroke_preserve(cr);
+    cairo_curve_to(cr, 5.0*w1, 2.6*h1, 4.0*w1, 2.8*h1, 3.0*w1, 3.8*h1);
+    cairo_stroke_preserve(cr);
+    cairo_close_path(cr);
+    cairo_stroke_preserve(cr);
+    cairo_fill(cr);
+    cairo_restore(cr);
+
+    //The electron.
+    cairo_save(cr);
+    cairo_set_source_rgb(cr, 0.6, 0.6, 1.0);
+    cairo_scale(cr, 1.0, 0.5);
+    cairo_arc(cr, 6.2*w1, 5.0*h1, 15*scale_line, 0, 2*G_PI);
+    cairo_fill(cr);
+    cairo_restore(cr);
+
+    //The electron line.
+    cairo_set_source_rgb(cr, 0.0, 0.0, 0.0);
+    cairo_set_line_width(cr, 2*scale_line);
+    cairo_move_to(cr, 6.0*w1, 2.5*h1);
+    cairo_line_to(cr, 6.4*w1, 2.5*h1);
+    cairo_stroke(cr);
+    
+    cairo_pattern_destroy(pattern1);
+  }
+
 
