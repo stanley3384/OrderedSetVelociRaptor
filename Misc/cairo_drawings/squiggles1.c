32efedb1ca11983ba04414beafd8a864eb4133c1
eric <cecashon@aol.com>
2017-06-17 15:21:31 -0700

    Add pointer smoothing to squiggles1.c

--- Misc/cairo_drawings/squiggles1.c
+++ Misc/cairo_drawings/squiggles1.c
@@ -27,15 +27,18 @@ struct controls{
 }controls;
 
 static void check_smooth(GtkToggleButton *check1, gpointer data);
+static void check_heap(GtkToggleButton *check2, gpointer data);
 static void button_clicked(GtkWidget *button, gpointer data);
 static gboolean da_drawing(GtkWidget *da, cairo_t *cr, gpointer data);
 static void draw_squiggles(GtkWidget *da, cairo_t *cr, gpointer data);
 static GArray* control_points_from_coords(GArray *dataPoints);
+static GArray* control_points_from_coords2(GArray *dataPoints);
 
 static GRand *rand1=NULL;
 static GArray *coords=NULL;
 
 gboolean draw_smooth=FALSE;
+gboolean use_heap=FALSE;
 
 int main(int argc, char *argv[])
   {
@@ -44,7 +47,7 @@ int main(int argc, char *argv[])
     GtkWidget *window=gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window), "Squiggles");
     gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
-    gtk_window_set_default_size(GTK_WINDOW(window), 400, 450);
+    gtk_window_set_default_size(GTK_WINDOW(window), 400, 500);
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
     rand1=g_rand_new_with_seed(time(NULL));
@@ -59,6 +62,10 @@ int main(int argc, char *argv[])
     gtk_widget_set_halign(check1, GTK_ALIGN_CENTER);
     g_signal_connect(check1, "toggled", G_CALLBACK(check_smooth), NULL);
 
+    GtkWidget *check2=gtk_check_button_new_with_label("Arrays on Heap");
+    gtk_widget_set_halign(check2, GTK_ALIGN_CENTER);
+    g_signal_connect(check2, "toggled", G_CALLBACK(check_heap), NULL);
+
     GtkWidget *button1=gtk_button_new_with_label("Redraw");
     gtk_widget_set_hexpand(button1, TRUE);
     gtk_widget_set_vexpand(button1, FALSE);
@@ -70,6 +77,7 @@ int main(int argc, char *argv[])
     gtk_grid_attach(GTK_GRID(grid), da, 0, 0, 1, 1);
     gtk_grid_attach(GTK_GRID(grid), button1, 0, 1, 1, 1);
     gtk_grid_attach(GTK_GRID(grid), check1, 0, 2, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), check2, 0, 3, 1, 1);
 
     gtk_container_add(GTK_CONTAINER(window), grid);
     gtk_widget_show_all(window);
@@ -85,6 +93,11 @@ static void check_smooth(GtkToggleButton *check1, gpointer data)
   {
     if(gtk_toggle_button_get_active(check1)) draw_smooth=TRUE;
     else draw_smooth=FALSE;
+  }
+static void check_heap(GtkToggleButton *check2, gpointer data)
+  {
+    if(gtk_toggle_button_get_active(check2)) use_heap=TRUE;
+    else use_heap=FALSE;
   }
 static void button_clicked(GtkWidget *button, gpointer data)
   {
@@ -121,10 +134,7 @@ static void draw_squiggles(GtkWidget *da, cairo_t *cr, gpointer data)
     g_array_remove_range(coords, 0, coords->len);
 
     gint i=0;
-    /*
-      Can change the number of points here but keep it reasonable. Also the control point arrays
-      are on the stack so don't abuse the privelege, too much.
-    */
+    //Change the number of points to test.  
     gint points=100;
     gdouble diff=0;
     struct point p1;
@@ -139,7 +149,6 @@ static void draw_squiggles(GtkWidget *da, cairo_t *cr, gpointer data)
         while(loop)
           {
             p1.x=g_rand_double(rand1)*width;
-            //g_print("x1 %f start %f back %f\n", x1, start_x, back_x);
             //Check if point is inside previous rectangle.
             if((start_x>back_x)&&(p1.x>start_x)) loop=FALSE;
             else if(p1.x<back_x) loop=FALSE;
@@ -167,7 +176,14 @@ static void draw_squiggles(GtkWidget *da, cairo_t *cr, gpointer data)
       }
 
     GArray *controlPoints=NULL;
-    if(draw_smooth) controlPoints=control_points_from_coords(coords);
+    if(draw_smooth)
+      {
+        GTimer *timer=g_timer_new();
+        if(use_heap) controlPoints=control_points_from_coords2(coords);
+        else controlPoints=control_points_from_coords(coords);
+        g_print("Time %f\n", g_timer_elapsed(timer, NULL));
+        g_timer_destroy(timer);
+      }
 
     //Draw the squiggley.
     gdouble red=0.0;
@@ -241,11 +257,12 @@ static void draw_squiggles(GtkWidget *da, cairo_t *cr, gpointer data)
     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
     SOFTWARE.
 
-    This is a translation of the original Swift code to C. First time looking at Swift for
-    me so there are some things that could be done better. There are a few arrays on the stack. 
+    This is a translation of the original Swift code to C. The first control_points_from_coords()
+    uses arrays on the stack. Version 2 control_points_from_coords2() puts the arrays on the heap. 
 */
 static GArray* control_points_from_coords(GArray *dataPoints)
   {  
+    g_print("Arrays on Stack\n");
     gint i=0;
     GArray *controlPoints=NULL;      
     //Number of Segments
@@ -397,7 +414,7 @@ static GArray* control_points_from_coords(GArray *dataPoints)
               }
 
           }
-       
+
         controlPoints=g_array_new(FALSE, FALSE, sizeof(struct controls));
         struct controls cp;
         for(i=0;i<count;i++)
@@ -411,6 +428,182 @@ static GArray* control_points_from_coords(GArray *dataPoints)
 
      }
 
+    return controlPoints;
+  }
+static GArray* control_points_from_coords2(GArray *dataPoints)
+  {  
+    g_print("Arrays on Heap\n");
+    gint i=0;
+    GArray *controlPoints=NULL;      
+    //Number of Segments
+    gint count=dataPoints->len-1;
+    gdouble *fCP=(gdouble*)g_malloc(2*count*sizeof(gdouble));
+    gdouble *sCP=(gdouble*)g_malloc(2*count*sizeof(gdouble));
+        
+    //P0, P1, P2, P3 are the points for each segment, where P0 & P3 are the knots and P1, P2 are the control points.
+    if(count==1)
+      {
+        struct point P0=g_array_index(dataPoints, struct point, 0);
+        struct point P3=g_array_index(dataPoints, struct point, 1);
+
+        //Calculate First Control Point
+        //3P1 = 2P0 + P3
+        struct point P1;
+        P1.x=(2.0*P0.x+P3.x)/3.0;
+        P1.y=(2.0*P0.y+P3.y)/3.0;
+
+        *(fCP)=P1.x;
+        *(fCP+1)=P1.y;
+
+        //Calculate second Control Point
+        //P2 = 2P1 - P0
+        struct point P2;
+        P2.x=(2.0*P1.x-P0.x);
+        P2.y=(2.0*P1.y-P0.x);
+
+        *(sCP)=P2.x;
+        *(sCP+1)=P2.y;      
+      }
+    else
+      {
+        gdouble *rhs=(gdouble*)g_malloc(2*count*sizeof(gdouble));
+        gdouble *a=(gdouble*)g_malloc(count*sizeof(gdouble));
+        gdouble *b=(gdouble*)g_malloc(count*sizeof(gdouble));
+        gdouble *c=(gdouble*)g_malloc(count*sizeof(gdouble));
+        gdouble rhsValueX=0;
+        gdouble rhsValueY=0;
+        struct point P0;
+        struct point P3;        
+        gdouble m=0;
+        gdouble b1=0;
+        gdouble r2x=0;
+        gdouble r2y=0;
+        gdouble P1_x=0;
+        gdouble P1_y=0;
+   
+        for(i=0;i<count;i++)
+          {
+            P0=g_array_index(dataPoints, struct point, i);
+            P3=g_array_index(dataPoints, struct point, i+1);
+
+            if(i==0)
+              {
+                *(a)=0.0;
+                *(b)=2.0;
+                *(c)=1.0;
+
+                //rhs for first segment
+                rhsValueX=P0.x+2.0*P3.x;
+                rhsValueY=P0.y+2.0*P3.y;
+              }
+            else if(i==count-1)
+              {
+                *(a+i)=2.0;
+                *(b+i)=7.0;
+                *(c+i)=0.0;
+
+                //rhs for last segment
+                rhsValueX=8.0*P0.x+P3.x;
+                rhsValueY=8.0*P0.y+P3.y;
+              }
+            else
+              {
+                *(a+i)=1.0;
+                *(b+i)=4.0;
+                *(c+i)=1.0;
+
+                rhsValueX=4.0*P0.x+2.0*P3.x;
+                rhsValueY=4.0*P0.y+2.0*P3.y;
+              }
+            *(rhs+i*2)=rhsValueX;
+            *(rhs+i*2+1)=rhsValueY;
+          }
+
+        //Solve Ax=B. Use Tridiagonal matrix algorithm a.k.a Thomas Algorithm
+        for(i=1;i<count;i++)
+          {
+            m=(*(a+i))/(*(b+i-1));
+
+            b1=(*(b+i))-m*(*(c+i-1));
+            *(b+i)=b1;
+
+            r2x=(*(rhs+i*2))-m*(*(rhs+(i-1)*2));
+            r2y=(*(rhs+i*2+1))-m*(*(rhs+(i-1)*2+1));
+
+            *(rhs+i*2)=r2x;
+            *(rhs+i*2+1)=r2y;
+          }
+
+        //Get First Control Points
+        
+        //Last control Point
+        gdouble lastControlPointX=(*(rhs+2*count-2))/(*(b+count-1));
+        gdouble lastControlPointY=(*(rhs+2*count-1))/(*(b+count-1));
+
+        *(fCP+2*count-2)=lastControlPointX;
+        *(fCP+2*count-1)=lastControlPointY;
+
+        gdouble controlPointX=0;
+        gdouble controlPointY=0;
+
+        for(i=count-2;i>=0;--i)
+          {
+            controlPointX=(*(rhs+i*2)-(*(c+i))*(*(fCP+(i+1)*2)))/(*(b+i));
+            controlPointY=(*(rhs+i*2+1)-(*(c+i))*(*(fCP+(i+1)*2+1)))/(*(b+i));
+
+             *(fCP+i*2)=controlPointX;
+             *(fCP+i*2+1)=controlPointY; 
+          }
+
+        //Compute second Control Points from first.
+        for(i=0;i<count;i++)
+          {
+            if(i==count-1)
+              {
+                P3=g_array_index(dataPoints, struct point, i+1);
+                P1_x=(*(fCP));
+                P1_y=(*(fCP+1));
+
+                controlPointX=(P3.x+P1_x)/2.0;
+                controlPointY=(P3.y+P1_y)/2.0;
+
+                *(sCP+count*2-2)=controlPointX;
+                *(sCP+count*2-1)=controlPointY;
+              }
+            else
+              {
+                P3=g_array_index(dataPoints, struct point, i+1);                
+                P1_x=(*(fCP+(i+1)*2));
+                P1_y=(*(fCP+(i+1)*2+1));
+
+                controlPointX=2.0*P3.x-P1_x;
+                controlPointY=2.0*P3.y-P1_y;
+
+                *(sCP+i*2)=controlPointX;
+                *(sCP+i*2+1)=controlPointY;
+              }
+
+          }
+
+        controlPoints=g_array_new(FALSE, FALSE, sizeof(struct controls));
+        struct controls cp;
+        for(i=0;i<count;i++)
+          {
+            cp.x1=(*(fCP+i*2));
+            cp.y1=(*(fCP+i*2+1));
+            cp.x2=(*(sCP+i*2));
+            cp.y2=(*(sCP+i*2+1));
+            g_array_append_val(controlPoints, cp);
+          }
+
+        g_free(rhs);
+        g_free(a);
+        g_free(b);
+        g_free(c);
+     }
+
+    g_free(fCP);
+    g_free(sCP);
     return controlPoints;
   }
 
