8a291181d65d1b85cb542da04c4095ae8e059eb2
eric <cecashon@aol.com>
2017-06-23 11:11:16 -0700

    Add a spiral to fish1.c

--- Misc/cairo_drawings/fish1.c
+++ Misc/cairo_drawings/fish1.c
@@ -1,6 +1,7 @@
 
 /* 
-    //Test drawing with generated control points. 
+    //Test drawing with generated control points. Draw a fish and a spiral. Getting the control
+points to draw the curves makes it much easier to draw something like a spiral.
 
     gcc -Wall fish1.c -o fish1 `pkg-config --cflags --libs gtk+-3.0` -lm
 
@@ -24,6 +25,8 @@ struct controls{
   gdouble y2;
 }controls;
 
+static gint combo_row=0;
+static void combo_changed(GtkComboBox *combo, gpointer data);
 static gboolean draw_fish(GtkWidget *da, cairo_t *cr, gpointer data);
 static GArray* control_points_from_coords2(const GArray *dataPoints);
 
@@ -41,9 +44,18 @@ int main(int argc, char **argv)
    gtk_widget_set_hexpand(da, TRUE);
    gtk_widget_set_vexpand(da, TRUE);
    g_signal_connect(da, "draw", G_CALLBACK(draw_fish), NULL);
+
+   GtkWidget *combo1=gtk_combo_box_text_new();
+   gtk_widget_set_hexpand(combo1, TRUE);
+   gtk_widget_set_vexpand(combo1, FALSE);
+   gtk_combo_box_text_insert(GTK_COMBO_BOX_TEXT(combo1), 0, "1", "Draw Fish");
+   gtk_combo_box_text_insert(GTK_COMBO_BOX_TEXT(combo1), 1, "2", "Draw Spiral");
+   gtk_combo_box_set_active(GTK_COMBO_BOX(combo1), 0);
+   g_signal_connect(combo1, "changed", G_CALLBACK(combo_changed), da);
    
    GtkWidget *grid=gtk_grid_new();
    gtk_grid_attach(GTK_GRID(grid), da, 0, 0, 1, 1);
+   gtk_grid_attach(GTK_GRID(grid), combo1, 0, 1, 1, 1);
    
    gtk_container_add(GTK_CONTAINER(window), grid);
 
@@ -53,8 +65,15 @@ int main(int argc, char **argv)
 
    return 0;  
  }
+static void combo_changed(GtkComboBox *combo, gpointer data)
+ {
+   combo_row=gtk_combo_box_get_active(combo);
+   gtk_widget_queue_draw(GTK_WIDGET(data)); 
+ }
 static gboolean draw_fish(GtkWidget *da, cairo_t *cr, gpointer data)
  {
+   gint i=0;
+   gint j=0;
    gdouble width=(gdouble)gtk_widget_get_allocated_width(da);
    gdouble height=(gdouble)gtk_widget_get_allocated_height(da);
    gdouble w1=width/10.0;
@@ -78,41 +97,66 @@ static gboolean draw_fish(GtkWidget *da, cairo_t *cr, gpointer data)
    //Sketch a few points for a fish.
    struct point p1;
    GArray *coords=g_array_new(FALSE, FALSE, sizeof(struct point));
-   cairo_set_source_rgb(cr, 0.0, 0.0, 1.0);
    cairo_set_line_width(cr, 3.0);
-   p1.x=9.0*w1;
-   p1.y=5.0*h1;
-   g_array_append_val(coords, p1);
-   p1.x=6.0*w1;
-   p1.y=3.0*h1;
-   g_array_append_val(coords, p1);
-   p1.x=2.5*w1;
-   p1.y=4.5*h1;
-   g_array_append_val(coords, p1);
-   p1.x=1.0*w1;
-   p1.y=3.0*h1;
-   g_array_append_val(coords, p1);
-   p1.x=1.25*w1;
-   p1.y=5.0*h1;
-   g_array_append_val(coords, p1);
-   p1.x=1.0*w1;
-   p1.y=7.0*h1;
-   g_array_append_val(coords, p1);
-   p1.x=2.5*w1;
-   p1.y=5.5*h1;
-   g_array_append_val(coords, p1);
-   p1.x=6.0*w1;
-   p1.y=7.0*h1;
-   g_array_append_val(coords, p1);
-   p1.x=9.0*w1;
-   p1.y=5.0*h1;
-   g_array_append_val(coords, p1);
+   if(combo_row==0)
+     {
+       cairo_set_source_rgb(cr, 0.0, 0.0, 1.0);
+       p1.x=9.0*w1;
+       p1.y=5.0*h1;
+       g_array_append_val(coords, p1);
+       p1.x=6.0*w1;
+       p1.y=3.0*h1;
+       g_array_append_val(coords, p1);
+       p1.x=2.5*w1;
+       p1.y=4.5*h1;
+       g_array_append_val(coords, p1);
+       p1.x=1.0*w1;
+       p1.y=3.0*h1;
+       g_array_append_val(coords, p1);
+       p1.x=1.25*w1;
+       p1.y=5.0*h1;
+       g_array_append_val(coords, p1);
+       p1.x=1.0*w1;
+       p1.y=7.0*h1;
+       g_array_append_val(coords, p1);
+       p1.x=2.5*w1;
+       p1.y=5.5*h1;
+       g_array_append_val(coords, p1);
+       p1.x=6.0*w1;
+       p1.y=7.0*h1;
+       g_array_append_val(coords, p1);
+       p1.x=9.0*w1;
+       p1.y=5.0*h1;
+       g_array_append_val(coords, p1);
+     }
+   else //Some points for a spiral.
+     {
+       cairo_translate(cr, width/2.0, height/2.0);
+       gdouble temp_x=0;
+       gdouble temp_y=0;
+       gdouble radius=4.0;
+       gdouble circle_points[]={0, G_PI/4.0, G_PI/2.0, 3.0*G_PI/4.0, G_PI, 5.0*G_PI/4.0, 3.0*G_PI/2.0, 7.0*G_PI/4.0};
+       gdouble spirals=10;
+       cairo_set_source_rgb(cr, 0.0, 1.0, 0.0);
+       cairo_set_line_width(cr, 3.0);
+       for(i=0;i<spirals;i++)
+         {
+           for(j=0; j<8; j++)
+             {
+               temp_x=radius*cos(circle_points[j]);
+               temp_y=radius*sin(circle_points[j]);
+               p1.x=temp_x*w1;
+               p1.y=temp_y*h1;
+               g_array_append_val(coords, p1);
+               radius-=.05;
+             }
+         }
+     }
 
-   //Get the control points for the fish.
+   //Get the control points.
    GArray *controlPoints=control_points_from_coords2(coords);
 
-   //Draw the fish.
-   gint i=0;
+   //Draw the curves.
    struct point d1;
    struct point d2;
    struct controls c1;   
@@ -126,8 +170,11 @@ static gboolean draw_fish(GtkWidget *da, cairo_t *cr, gpointer data)
        cairo_stroke(cr);
      }
 
-   cairo_arc(cr, 7.5*w1, 4.25*h1, 0.10*h1, 0.0, 2.0*G_PI);
-   cairo_fill(cr);  
+   if(combo_row==0)
+     {
+       cairo_arc(cr, 7.5*w1, 4.25*h1, 0.10*h1, 0.0, 2.0*G_PI);
+       cairo_fill(cr);
+     }  
    
    g_array_free(coords, TRUE);
    g_array_free(controlPoints, TRUE);
